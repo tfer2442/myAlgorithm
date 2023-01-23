04ae9df0eb0393bf8dd1b819615632fb95983291
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-01-23 17:34:47 +0900

    [Silver III] Title: 피보나치 함수, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -7,7 +7,7 @@ for i in range(T):
     dp[0] = 1
     dp[1] = 0
 
-    for j in range(2, n + 2):
-        dp[j] = dp[j - 1] + dp[j - 2]
+    for j in range(2, n + 2): dp[j] = dp[j - 1] + dp[j - 2]
 
     print(dp[n], dp[n+1])
+    
