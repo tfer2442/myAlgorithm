4c06a631fdf477e5948ad240aeb913eadc3466a5
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-01-24 13:29:04 +0900

    [Silver I] Title: 미로 탐색, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,45 @@
+def solution(maps):
+    answer = 0
+    r = len(maps)
+    c = len(maps[0])
+    q = []
+
+    a = 0
+    b = 0
+    q.append([a, b])
+
+    while True:
+        if len(q) == 0:
+            return -1
+        a, b = q.pop(0)
+        #maps[a][b] = 0
+
+        if a+1 != r and maps[a+1][b] == 1 and not(a+1 == 0 and b == 0):
+            maps[a+1][b] = maps[a][b] + 1
+            q.append([a+1, b])
+        if b+1 != c and maps[a][b+1] == 1 and not(a == 0 and b+1 == 0):
+            maps[a][b+1] = maps[a][b] + 1
+            q.append([a, b+1])
+        if a-1 != -1 and maps[a-1][b] == 1 and not(a-1 == 0 and b == 0):
+            maps[a-1][b] = maps[a][b] + 1
+            q.append([a-1, b])
+        if b-1 != -1 and maps[a][b-1] == 1 and not(a == 0 and b-1 == 0):
+            maps[a][b-1] = maps[a][b] + 1
+            q.append([a, b-1])
+
+        if a == r-1 and b == c-1:
+            answer = maps[r-1][c-1]
+            break
+
+    return answer
+
+
+n, m = map(int, input().split())
+maps = [[0] * m for i in range(n)]
+
+for i in range(n):
+    str1 = input()
+    for j in range(m):
+        maps[i][j] = int(str1[j])
+
+print(solution(maps))
