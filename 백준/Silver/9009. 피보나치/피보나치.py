c78dd4a150b73e37f359e1c9dd6ec2775bf23de2
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-01-28 04:59:17 +0900

    [Silver I] Title: 피보나치, Time: 52 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/9009. 피보나치/피보나치.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+dp = [0] * 51
+dp[1] = 1
+
+for i in range(2, 51):
+    dp[i] = dp[i-1] + dp[i-2]
+
+for i in range(t):
+    n = int(input())
+    s = []
+    for j in range(50, 0, -1):
+        if n-dp[j] < 0:
+            pass
+        elif n-dp[j] > 0:
+            n = n - dp[j]
+            s.append(dp[j])
+        else:
+            s.append(dp[j])
+            break
+    for j in reversed(s):
+        print(j, end=' ')
+    print()
+
