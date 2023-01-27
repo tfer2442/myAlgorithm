4c3e83411e66da249ee91607c5fc8eb90655a5b7
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-01-28 05:03:13 +0900

    [Silver I] Title: 피보나치, Time: 52 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/9009. 피보나치/피보나치.py
+++ 백준/Silver/9009. 피보나치/피보나치.py
@@ -3,16 +3,16 @@ input = sys.stdin.readline
 
 t = int(input())
 
-dp = [0] * 51
+dp = [0] * 46
 dp[1] = 1
 
-for i in range(2, 51):
+for i in range(2, 46):
     dp[i] = dp[i-1] + dp[i-2]
 
 for i in range(t):
     n = int(input())
     s = []
-    for j in range(50, 0, -1):
+    for j in range(45, 0, -1):
         if n-dp[j] < 0:
             pass
         elif n-dp[j] > 0:
