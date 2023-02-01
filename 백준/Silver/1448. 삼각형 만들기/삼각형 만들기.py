2e6be9a9d0c7a000fe99cf99ab1ee7354554f629
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-02-01 20:31:16 +0900

    [Silver III] Title: 삼각형 만들기, Time: 660 ms, Memory: 76132 KB -BaekjoonHub

---
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+l1 = []
+for i in range(n):
+    l1.append(int(input()))
+l1.sort()
+
+c = len(l1) - 1
+b = c - 1
+a = b - 1
+
+while True:
+    if l1[a] + l1[b] > l1[c]:
+        print(l1[a]+l1[b]+l1[c])
+        break
+    else:
+        c = b
+        b = a
+        if a - 1 < 0:
+            print(-1)
+            break
+        a = a - 1
