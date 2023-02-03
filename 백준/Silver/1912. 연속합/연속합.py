5b948ab695d6df308fdbce3543a04563f895608a
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-02-03 23:18:43 +0900

    [Silver II] Title: 연속합, Time: 96 ms, Memory: 38964 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+s = []
+
+l1 = list(map(int, input().split()))
+
+cnt = 0
+iSum = 0
+
+if n == 1:
+    s.append(l1[0])
+
+for i in range(n):
+    if l1[i] < 0:
+        cnt += 1
+    if l1[i] > 0:
+        cnt -= 1
+        
+    if l1[i] >= 0:
+        iSum = iSum + l1[i]
+    elif l1[i] < 0 and iSum < abs(l1[i]):
+        s.append(iSum)
+        iSum = 0
+    elif l1[i] < 0:
+        s.append(iSum)
+        iSum = iSum + l1[i]
+
+if cnt == n:
+    print(max(l1))
+elif cnt == -n:
+    print(sum(l1))
+else:
+    print(max(s))
