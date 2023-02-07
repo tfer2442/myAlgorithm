3f52f4bc62bfc83633f2c8b9166c4486c6b51fdb
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-02-08 01:25:39 +0900

    [Silver III] Title: 문자열 집합, Time: 124 ms, Memory: 42352 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,14 +1,17 @@
+# list, tuple로 in 사용하면 O(n)이다.
+# set, dictionary로 사용하면 평균 O(1) 최악 O(n)이다. hash로 값을 저장하기 때문에
+
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-s = []
+s = set()
 check = []
 count = 0
 
 for i in range(n):
-    s.append(input())
+    s.add(input())
 
 for i in range(m):
     check.append(input())
