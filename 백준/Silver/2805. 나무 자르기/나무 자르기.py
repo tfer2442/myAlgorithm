6f991fb3033fa4c2b8d45dc4acc5eaed344bcdcc
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-02-09 03:28:47 +0900

    [Silver II] Title: 나무 자르기, Time: 572 ms, Memory: 259740 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+l1 = list((map(int, input().split())))
+
+left = 0
+right = max(l1)
+mid = (left + right) // 2
+
+while True:
+    if left >= right:
+        print(mid)
+        break
+
+    Tsum = 0
+    for i in range(n):
+        if l1[i] - mid >= 0:
+            Tsum += l1[i] - mid
+
+    if Tsum < m:
+        right = mid
+        mid = (right+left) // 2
+
+    elif Tsum > m:
+        left = mid
+        mid = (right+left) // 2
+        if left == mid:
+            print(mid)
+            break
+    else:
+        print(mid)
+        break
+
