306d6e47a443fc85ca2e2b78d5844219ad7024ef
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-02-11 01:31:43 +0900

    [Silver II] Title: 스타트와 링크, Time: 2960 ms, Memory: 65432 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -10,21 +10,17 @@ sum1 = [0]*(len(l2)//2)
 sum2 = [0]*(len(l2)//2)
 
 cnt = 0
-for i in l2[0:len(l2)//2]:
-    for j in i:
-        for k in i:
+for i, a in zip(l2[0:len(l2)//2], l2[len(l2)//2:len(l2)]):
+    for j, b in zip(i, a):
+        for k, c in zip(i, a):
             sum1[cnt] += l1[j][k]
+            sum2[cnt] += l1[b][c]
     cnt += 1
 
-cnt = 0
-for i in l2[len(l2)//2:len(l2)]:
-    for j in i:
-        for k in i:
-            sum2[cnt] += l1[j][k]
-    cnt += 1
 
 result = 101
 for i in range(len(sum1)):
     result = min(result, abs(sum1[i] - sum2[len(sum1)-i-1]))
 
 print(result)
+
