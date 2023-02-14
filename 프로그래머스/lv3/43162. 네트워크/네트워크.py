d1af20a531c0eb707913c37c8c94967be0adb259
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-02-15 03:00:31 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -0,0 +1,31 @@
+from collections import deque
+# 그래프 만드는 법 복습!!
+def solution(n, computers):
+    l1 = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i != j and computers[i][j]:
+                l1[i].append(j)
+    q = deque()
+    visited = [False] * n
+    cnt = 0
+
+    for j in range(n):
+        if visited[j]:
+            continue
+        q.append(j)
+        visited[j] = True
+        cnt = cnt + 1
+        while True:
+            if not q:
+                break
+            a = q.popleft()
+
+            for i in l1[a]:
+                if not visited[i]:
+                    q.append(i)
+                    visited[i] = True
+
+
+    answer = cnt
+    return answer
