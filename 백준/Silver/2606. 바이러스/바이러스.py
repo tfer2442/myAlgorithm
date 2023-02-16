7cd9fb0211002e6d4b7e1f8b64ecac5905c422cf
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-02-16 15:24:05 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 34104 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,23 @@
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+
+def dfs(cnt):
+    for i in graph[cnt]:
+        if not visited[i]:
+            visited[i] = True
+            dfs(i)
+
+
+n = int(input())
+eNum = int(input())
+graph = [[] for _ in range(n+1)]
+
+for i in range(eNum):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+visited = [False] * (n+1)
+dfs(1)
+print(Counter(visited)[True] - 1)
