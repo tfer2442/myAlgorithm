644f53bbdadfc75ea4a24d07417f042a1d022609
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-02-22 04:29:54 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 116 ms, Memory: 35928 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+def bfs():
+    if n >= k:
+        print(n - k)
+        return
+    else:
+        while True:
+            if visited[k]:
+                print(visited[k] - 1)
+                return
+            tmp = l1.popleft()
+
+            for i in [tmp * 2, tmp-1, tmp+1]:
+                if 0 <= i <= 200000 and not visited[i]:
+                    l1.append(i)
+                    if i != tmp * 2:
+                        visited[i] = visited[tmp] + 1
+                    else:
+                        visited[i] = visited[tmp]
+
+
+n, k = map(int, input().split())
+
+l1 = deque()
+visited = [0]*200001
+l1.append(n)
+visited[n] = 1
+bfs()
