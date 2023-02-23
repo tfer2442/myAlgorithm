ccc11c7d6b1dab1fbe12e2af26ef63d5a66c752b
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-02-23 20:14:06 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -3,8 +3,6 @@ from itertools import combinations
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-l1 = []
-for i in range(1, n+1):
-    l1.append(i)
+l1 = [i for i in range(1, n+1)]
 for i in combinations(l1, m):
     print(*i)
