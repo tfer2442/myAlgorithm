03297dd96b7ab0e2f950e8b39e71d18522e4085d
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-02-24 20:52:44 +0900

    [Gold IV] Title: RGB거리 2, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.py
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -9,9 +9,9 @@ for j in range(3):
     dp = [[9999, 9999, 9999] for _ in range(n)]
     dp[0][j] = l1[0][j]
     for i in range(1, n):
-        dp[i][0] = min(l1[i][0] + dp[i-1][1], l1[i][0] + dp[i - 1][2])
-        dp[i][1] = min(l1[i][1] + dp[i-1][0], l1[i][1] + dp[i - 1][2])
-        dp[i][2] = min(l1[i][2] + dp[i-1][1], l1[i][2] + dp[i - 1][0])
+        dp[i][0] = l1[i][0] + min(dp[i-1][1], dp[i - 1][2])
+        dp[i][1] = l1[i][1] + min(dp[i-1][0], dp[i - 1][2])
+        dp[i][2] = l1[i][2] + min(dp[i-1][1], dp[i - 1][0])
 
     for i in range(3):
         if i != j:
