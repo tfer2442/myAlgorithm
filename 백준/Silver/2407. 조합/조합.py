bcd5e96d916180fc5e911286214fc4e767801f53
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-02-26 02:51:56 +0900

    [Silver III] Title: 조합, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.py
+++ 백준/Silver/2407. 조합/조합.py
@@ -1,17 +1,11 @@
 import sys
-from itertools  import combinations
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-l = [[0]*101 for _ in range(101)]
-for i in range(1, 101):
-    l[i][0] = 1
-l[1][1] = 1
+l = [[1]*101 for _ in range(101)]
+
 for i in range(2, 101):
-    for j in range(1, i+1):
-        if l[i-1][j] == 0:
-            l[i][j] = 1
-            break
+    for j in range(1, i):
         l[i][j] = l[i-1][j-1] + l[i-1][j]
 print(l[n][m])
