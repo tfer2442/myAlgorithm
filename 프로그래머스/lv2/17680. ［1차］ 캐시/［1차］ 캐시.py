570a3742c20f8cdc941808e8707874d5ee3d04f5
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-05-17 23:02:59 +0900

    [level 2] Title: [1차] 캐시, Time: 0.91 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,21 @@
+def solution(cacheSize, cities):
+    l1 = []
+    answer = 0
+    for i in range(len(cities)):
+        cities[i] = cities[i].lower()
+        
+    for i in range(len(cities)):
+        if len(l1) > cacheSize:
+            l1.pop(-1)
+        if cities[i] in l1:
+            tmp = l1.index(cities[i])
+            l1.pop(tmp)
+            l1.insert(0, cities[i])
+            answer += 1
+        else:
+            l1.insert(0, cities[i])
+            answer += 5
+
+        
+    
+    return answer
