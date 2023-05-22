677b05194f32273f7c04971201a1ae37d4a20233
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-05-22 23:48:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 58.81 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -12,7 +12,7 @@ def solution(queue1, queue2):
     while True:
         if not queue1 or not queue2:
             return -1
-        if cnt >= (lenq * 3):
+        if cnt >= (lenq * 3) + 2:
             return -1
         if sq2 > sq1:
             tmp = queue2.popleft()
