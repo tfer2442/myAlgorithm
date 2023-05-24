6b40462f0ae4f157dbafa5bce4c2f32c27fa588e
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-05-24 19:24:23 +0900

    [unrated] Title: 리코쳇 로봇, Time: 0.62 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -0,0 +1,54 @@
+def solution(board):
+    answer = 0
+    d1 = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1)}
+
+    maxr = len(board)
+    maxc = len(board[0])
+    flag = 0
+    visited = [[0]*maxc for _ in range(maxr)]
+    for i in range(maxr):
+        for j in range(maxc):
+            if board[i][j] == 'R':
+                sr, sc = i, j
+                flag = 1
+                break
+        if flag == 1: break
+
+    q = []
+    q.append((sr, sc, 0))
+    visited[sr][sc] = 1
+    while True:
+        if not q:
+            answer = -1
+            break
+
+        a, b, cnt = q.pop(0)
+
+        for i in d1:
+            tmpa = a
+            tmpb = b
+
+            while True:
+                c, d = tmpa + d1[i][0], tmpb + d1[i][1]
+
+                if (0 <= c < maxr) and (0 <= d < maxc):
+                    if board[c][d] != 'D':
+                        tmpa, tmpb = c, d
+                    else:
+                        if board[tmpa][tmpb] == 'G':
+                            return cnt+1
+                        break
+                else:
+                    if board[tmpa][tmpb] == 'G':
+                        return cnt+1
+                    break
+            if (a != tmpa) or (b != tmpb):
+                if visited[tmpa][tmpb] == 1:
+                    continue
+                visited[tmpa][tmpb] = 1
+                q.append((tmpa, tmpb, cnt + 1))
+
+    return answer
+
+
+print(solution([".D.R", "....", ".G..", "...D"]))
