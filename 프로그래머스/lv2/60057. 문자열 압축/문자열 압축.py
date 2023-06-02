a9afd69d433047728b1b973f8463537276370f8e
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-06-02 22:23:04 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,23 @@
+def solution(s):
+    answer = 0
+    minlist = []
+    for i in range(1, len(s)):
+        tmps = ""
+        cnt = 1
+
+        for j in range(0, len(s), i):
+            if s[j:j+i] == s[j+i:j+i+i]:
+                cnt += 1           
+            else:
+                if cnt == 1:
+                    tmps += s[j:j+i]
+                else:
+                    tmps += str(cnt) + s[j:j+i]
+                cnt = 1
+        minlist.append(len(tmps))
+    if minlist:
+        answer = min(minlist)
+    else:
+        answer = len(s)
+         
+    return answer
