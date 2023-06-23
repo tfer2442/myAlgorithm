c8ec56f65fc6c7078c85105ae51c7d25e98f9fae
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-06-24 00:30:32 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,23 +1,20 @@
 def solution(progresses, speeds):
     answer = []
 
-    cnt = 0
     while progresses:
         for i in range(len(progresses)):
             progresses[i] = progresses[i] + speeds[i]
 
-        if progresses[0] >= 100:
-            for j in range(len(progresses)):
-                if progresses[j] < 100:
-                    for k in range(j):
-                        progresses.pop(0)
-                        speeds.pop(0)
-                    answer.append(j)
-                    break
-            else:
-                for k in range(j+1):
+        cnt = 0
+        while progresses:
+            if progresses[0] >= 100:
                     progresses.pop(0)
                     speeds.pop(0)
-                answer.append(j+1)
+                    cnt += 1
+            else:
+
+                break
+        if cnt:
+            answer.append(cnt)
 
     return answer
