9231cbd1abcf990b2f314b7cfc076adc9abbbc61
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-11-22 22:41:27 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 124.39 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -0,0 +1,44 @@
+def isBombBlock(board, i, j):
+    if board[i][j] == '!':
+        return False
+    
+    if board[i][j] == board[i][j+1] == board[i+1][j] == board[i+1][j+1]:
+        return True
+    return False
+
+def solution(m, n, board):
+    answer = 0
+    
+    boardList = [[0]*m for _ in range(n)]
+
+    for j in range(n):
+        for i in range(m):
+            boardList[n-j-1][i] = board[i][j]
+    
+    for _ in range(m*n):
+        bombIndexes = []
+        for i in range(n-1):
+            for j in range(m-1):
+                if isBombBlock(boardList, i, j):
+                    bombIndexes.append((i,j))
+        
+        for index in bombIndexes:
+            boardList[index[0]][index[1]] = '!'
+            boardList[index[0]+1][index[1]] = '!'
+            boardList[index[0]][index[1]+1] = '!'
+            boardList[index[0]+1][index[1]+1] = '!'
+    
+        for i in range(n):
+            tmp = ''.join(boardList[i])
+            tmp = tmp.replace('!', '')
+            tmp = tmp.rjust(m, '!')
+            #tmp = " "*(m-len(tmp)) + tmp
+            boardList[i] = list(tmp[:])
+    
+    
+    for i in range(n):
+        for j in range(m):
+            if not boardList[i][j].isalpha():
+                answer += 1
+                
+    return answer
