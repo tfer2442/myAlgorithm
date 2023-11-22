c6edeac95226631c4ac21115f39b0748a6f7f6d8
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-11-22 21:08:35 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.60 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,41 @@
+splitedFile = []
+
+def splitFile(index, file):
+    head = ""
+    number = ""
+    tail = ""
+    tailIndex = 0
+    flag = False
+    
+    for i, s in enumerate(file):
+        if s.isdigit():
+            flag = True
+            number += s
+        elif flag == False:
+            head += s
+        elif flag == True:
+            tailIndex = i
+            break
+    
+    if tailIndex == 0:
+        splitedFile.append([head.upper(), int(number), tail, index])
+    else:
+        splitedFile.append([head.upper(), int(number), file[tailIndex:], index])
+
+        
+        
+def solution(files):
+    answer = []
+    sortingIndex = []
+    
+    for i, file in enumerate(files):
+        splitFile(i, file)
+    splitedFile.sort(key = lambda x : (x[0], x[1]))
+    
+    for i in range(len(splitedFile)):
+        sortingIndex.append(splitedFile[i][3])
+    
+    for i in sortingIndex:
+        answer.append(files[i])
+    
+    return answer
