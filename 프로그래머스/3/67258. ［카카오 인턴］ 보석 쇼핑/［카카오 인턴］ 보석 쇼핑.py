4e264cef22b7e9dec4a724d5e6121ae364aab6ef
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2023-11-23 21:50:39 +0900

    Update ［카카오 인턴］ 보석 쇼핑.py

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -6,34 +6,31 @@ def solution(gems):
     allGems = Counter(gems)
     gemTypeNum = len(allGems)
     basket = Counter()
-    gems.append(0)
-    basket[gems[0]] = 1
     start = 0
+    basket[gems[start]] = 1
     i = 1
     
     if gemTypeNum == 1:
         return [1, 1]
         
-    
     while True:
-        if i >= len(gems):
-            break
-            
         if len(basket) == gemTypeNum:
             answer.append([start+1, i])
             basket[gems[start]] -= 1
             if basket[gems[start]] == 0:
                 del basket[gems[start]]
-        
             start += 1
             continue
+        if i >= len(gems):
+            break
             
         if gems[start] == gems[i]:
             start += 1
         else:
             basket[gems[i]] += 1
-        i += 1
-    
+        i += 1    
+          
+        
     answer.sort(key = lambda x : ((x[1]-x[0]), x[0]))
     
     return answer[0]
