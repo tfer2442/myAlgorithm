e5a847acdf3d47605284b7fced29bba0eaf0e077
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-01-28 21:15:24 +0900

    [Silver I] Title: 회전 초밥, Time: 108 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -6,19 +6,29 @@ input = sys.stdin.readline
 n, d, k, c = map(int, input().split())
 result = []
 belt = []
+sushi = Counter()
 
 for _ in range(n):
     belt.append(int(input()))
 
-for i in range(n):
-    sushi = Counter()
-    for j in range(k):
-        if i + j >= n:
-            sushi[belt[i+j-n]] += 1
-        else:
-            sushi[belt[i+j]] += 1
-    sushi[c] += 1
-    result.append(len(sushi))
+for i in range(k):
+    sushi[belt[i]] += 1
+sushi[c] += 1
+result.append(len(sushi))
 
+for i in range(k, n+k-1):
+    if i >= n:
+        sushi[belt[i-n]] += 1
+        sushi[belt[i-n-k]] -= 1
+        tmp = belt[i-n-k]
+    else:
+        sushi[belt[i]] += 1
+        sushi[belt[i-k]] -= 1
+        tmp = belt[i-k]
+
+    if sushi[tmp] == 0:
+        del sushi[tmp]
+
+    result.append(len(sushi))
 
 print(max(result))
