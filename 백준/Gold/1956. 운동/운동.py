dad95200cb9f78914262af97e7b872490b076767
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-01-31 23:39:54 +0900

    [Gold IV] Title: 운동, Time: 1444 ms, Memory: 113024 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -5,12 +5,6 @@ input = sys.stdin.readline
 
 INF = 1e9
 
-
-def initGraph(v, graph):
-    for i in range(1, v+1):
-        graph[i][i] = 0
-
-
 def updateMinLength(v, graph):
     for k in range(1, v+1):
         for i in range(1, v+1):
@@ -25,8 +19,6 @@ def updateMinLength(v, graph):
 v, e = map(int, input().split())
 graph = [[INF] * (v+1) for _ in range(v+1)]
 
-initGraph(v, graph)
-
 for _ in range(e):
     a, b, c = map(int, input().split())
     graph[a][b] = c
@@ -34,10 +26,10 @@ for _ in range(e):
 updateMinLength(v, graph)
 cycleLength = []
 
-for start, end in combinations(range(1, v+1), 2):
-    if graph[start][end] == INF or graph[end][start] == INF:
+for i in range(1, v+1):
+    if graph[i][i] == INF:
         continue
-    cycleLength.append(graph[start][end] + graph[end][start])
+    cycleLength.append(graph[i][i])
 
 if cycleLength:
     print(min(cycleLength))
