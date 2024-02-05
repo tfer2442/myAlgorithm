a44c8838689e442c575ba2413feaec13e2b8ee6c
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-02-05 16:31:53 +0900

    [Bronze III] Title: ZOAC 4, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
@@ -0,0 +1,11 @@
+import sys
+import math
+
+input = sys.stdin.readline
+
+h, w, n, m = map(int, input().split())
+
+rowPeople = math.ceil(w / (m + 1))
+colPeople = math.ceil(h / (n + 1))
+
+print(rowPeople * colPeople)
