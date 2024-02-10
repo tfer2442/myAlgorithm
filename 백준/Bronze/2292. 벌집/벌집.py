05e8817da42259826a08d228a77d5c34473028ac
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-02-10 17:08:41 +0900

    [Bronze II] Title: 벌집, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,25 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-
-beeHouse = [1, 2]
-start = 8
-d = 12
-
-while True:
-    if start > 1_000_000_000:
-        beeHouse.append(start)
-        break
-    beeHouse.append(start)
-    start += d
-    d += 6
-
 n = int(input())
 
-if n == 1:
-    print(1)
-else:
-    for i in range(1, len(beeHouse)):
-        if beeHouse[i-1] <= n < beeHouse[i]:
-            print(i)
-            break
+cnt = 1
+d = 0
+maxValue = 1
+
+while True:
+    if n <= maxValue:
+        print(cnt)
+        break
+    cnt += 1
+    d += 6
+    maxValue += d
