a13fc2c75e1014c0dea53fb7d1237b9b0e8d5e29
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-02-22 02:01:56 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 424 ms, Memory: 38008 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -3,36 +3,40 @@ from collections import deque
 input = sys.stdin.readline
 
 
-n, m = map(int, input().split())
-mapInfo = list()
-start = 0
+def solution():
+    mapInfo = list()
+    start = 0
 
-for i in range(n):
-    mapInfo.append(list(map(int, input().split())))
+    for i in range(n):
+        mapInfo.append(list(map(int, input().split())))
 
-    if start == 0:
+        if start == 0:
+            for j in range(m):
+                if mapInfo[i][j] == 2:
+                    start = (i, j)
+
+    q = deque({start})
+
+    d = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+    while q:
+        y, x = q.popleft()
+
+        for a, b in d:
+            r, c = y + a, x + b
+
+            if 0 <= r < n and 0 <= c < m and mapInfo[r][c] == 1:
+                mapInfo[r][c] = mapInfo[y][x] + 1
+                q.append((r, c))
+
+    for i in range(n):
         for j in range(m):
-            if mapInfo[i][j] == 2:
-                start = (i, j)
+            if mapInfo[i][j] == 0:
+                print(mapInfo[i][j], end=' ')
+            else:
+                print(mapInfo[i][j] - 2, end=' ')
+        print()
 
-q = deque({start})
 
-d = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-
-while q:
-    y, x = q.popleft()
-
-    for a, b in d:
-        r, c = y + a, x + b
-
-        if 0 <= r < n and 0 <= c < m and mapInfo[r][c] == 1:
-            mapInfo[r][c] = mapInfo[y][x] + 1
-            q.append((r, c))
-
-for i in range(n):
-    for j in range(m):
-        if mapInfo[i][j] == 0:
-            print(mapInfo[i][j], end=' ')
-        else:
-            print(mapInfo[i][j]-2, end=' ')
-    print()
+n, m = map(int, input().split())
+solution()
