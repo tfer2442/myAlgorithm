0d29eb197f181b545561c8b437e67402b18ab9a5
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-02-25 19:05:55 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -2,51 +2,31 @@ import sys
 input = sys.stdin.readline
 
 
-def isMovePipe(row, col, d):
-    if row >= n or col >= n:
-        return False
-    if d == 3:
-        if house[row-1][col] == 1 or house[row][col-1] == 1:
-            return False
-    if house[row][col] == 0:
-        return True
-    if house[row][col] == 1:
-        return False
-
-
-def dfs(end, d):
-    global cnt
-
-    if end[0] == n-1 and end[1] == n-1:
-        cnt += 1
-        return
-
-    if d == 1:
-        if isMovePipe(end[0], end[1]+1, 1):  # 가로
-            dfs((end[0], end[1]+1), 1)
-        if isMovePipe(end[0]+1, end[1]+1, 3):  # 대각선
-            dfs((end[0]+1, end[1]+1), 3)
-    elif d == 2:
-        if isMovePipe(end[0]+1, end[1], 2):  # 세로
-            dfs((end[0]+1, end[1]), 2)
-        if isMovePipe(end[0]+1, end[1]+1, 3):  # 대각선
-            dfs((end[0]+1, end[1]+1), 3)
-    elif d == 3:
-        if isMovePipe(end[0]+1, end[1], 2):  # 세로
-            dfs((end[0]+1, end[1]), 2)
-        if isMovePipe(end[0], end[1]+1, 1):  # 가로
-            dfs((end[0], end[1]+1), 1)
-        if isMovePipe(end[0]+1, end[1]+1, 3):  # 대각선
-            dfs((end[0]+1, end[1]+1), 3)
-
-
 n = int(input())
 house = [list(map(int, input().split())) for _ in range(n)]
+dp = [[[0] * 3 for _ in range(n)] for _ in range(n)]
 if house[n-1][n-1] == 1:
     print(0)
     exit(0)
-start = (0, 1)
-cnt = 0
-dfs(start, 1)
-print(cnt)
 
+for i in range(1, n):
+    if house[0][i] == 1:
+        break
+    dp[0][i][0] = 1
+
+for i in range(1, n):
+    for j in range(2, n):
+
+        if house[i][j] == 1:
+            continue
+
+        if house[i][j-1] != 1:
+            dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2]
+
+        if house[i-1][j] != 1:
+            dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2]
+
+        if house[i-1][j-1] != 1 and house[i][j-1] != 1 and house[i-1][j] != 1:
+            dp[i][j][2] = sum(dp[i-1][j-1])
+
+print(sum(dp[n-1][n-1]))
