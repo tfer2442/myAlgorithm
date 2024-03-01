89fd6dae40e46aa039428b8e5e313f03aaf1f2d2
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-03-02 02:27:33 +0900

    [Silver III] Title: 햄버거 분배, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -1,42 +1,47 @@
 import sys
 input = sys.stdin.readline
 
+
+def solution():
+    hamburger = list()
+    people = list()
+
+    for i, element in enumerate(table):
+        if element == 'H':
+            hamburger.append(i)
+            continue
+        if element == 'P':
+            people.append(i)
+            continue
+
+    cnt = 0
+    hamburgerIndex = 0
+    peopleIndex = 0
+
+    while hamburgerIndex < len(hamburger) and peopleIndex < len(people):
+        if 0 < people[peopleIndex] - hamburger[hamburgerIndex] <= k:
+            hamburgerIndex += 1
+            peopleIndex += 1
+            cnt += 1
+            continue
+
+        if k < people[peopleIndex] - hamburger[hamburgerIndex]:
+            hamburgerIndex += 1
+            continue
+
+        if 0 < hamburger[hamburgerIndex] - people[peopleIndex] <= k:
+            hamburgerIndex += 1
+            peopleIndex += 1
+            cnt += 1
+            continue
+
+        if k < hamburger[hamburgerIndex] - people[peopleIndex]:
+            peopleIndex += 1
+            continue
+    return cnt
+
+
 n, k = map(int, input().split())
 table = input().strip()
-hamburger = list()
-people = list()
 
-for i, element in enumerate(table):
-    if element == 'H':
-        hamburger.append(i)
-        continue
-    if element == 'P':
-        people.append(i)
-        continue
-
-cnt = 0
-hamburgerIndex = 0
-peopleIndex = 0
-
-while hamburgerIndex < len(hamburger) and peopleIndex < len(people):
-    if 0 < people[peopleIndex] - hamburger[hamburgerIndex] <= k:
-        hamburgerIndex += 1
-        peopleIndex += 1
-        cnt += 1
-        continue
-
-    if k < people[peopleIndex] - hamburger[hamburgerIndex]:
-        hamburgerIndex += 1
-        continue
-
-    if 0 < hamburger[hamburgerIndex] - people[peopleIndex] <= k:
-        hamburgerIndex += 1
-        peopleIndex += 1
-        cnt += 1
-        continue
-
-    if k < hamburger[hamburgerIndex] - people[peopleIndex]:
-        peopleIndex += 1
-        continue
-
-print(cnt)
+print(solution())
