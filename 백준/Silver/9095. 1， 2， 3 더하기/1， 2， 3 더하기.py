6e32fb6ab5714d4b5be26ce74e5dc5611f28cdc8
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-03-01 22:17:19 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,20 +1,14 @@
 import sys
-from itertools import product
 input = sys.stdin.readline
 
 T = int(input())
 
-for _ in range(T):
-    l1 = []
-    l2 = []
+for i in range(T):
     n = int(input())
-
-    for i in range(1, n):
-        l1 = list(product([1, 2, 3], repeat=i))
-        for j in l1:
-            l2.append(j)
-    cnt = 1
-    for i in l2:
-        if n == sum(i):
-            cnt = cnt + 1
-    print(cnt)
+    dp = [0, 1, 2, 4, 7]
+    if n <= 4:
+        print(dp[n])
+        continue
+    for j in range(5, n+1):
+        dp.append(dp[j-3] + dp[j-2] + dp[j-1])
+    print(dp[n])
