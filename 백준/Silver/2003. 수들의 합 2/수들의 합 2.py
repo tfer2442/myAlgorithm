0e0fb5353de382bcb801991a363feb0da96e3da4
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-03-25 17:18:54 +0900

    [Silver IV] Title: 수들의 합 2, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -5,34 +5,16 @@ n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 
 left = 0
-right = 1
-subTotal = nums[0]
 cnt = 0
+subTotal = 0
 
-while left <= right:
-    if left >= n:
-        break
+for right in range(n):
+    subTotal += nums[right]
+    while subTotal > m and left <= right:
+        subTotal -= nums[left]
+        left += 1
 
     if subTotal == m:
         cnt += 1
-        if right >= n:
-            break
-        subTotal -= nums[left]
-        subTotal += nums[right]
-        left += 1
-        right += 1
-        continue
-
-    if subTotal > m:
-        subTotal -= nums[left]
-        left += 1
-        continue
-
-    if subTotal < m:
-        if right >= n:
-            break
-        subTotal += nums[right]
-        right += 1
-        continue
 
 print(cnt)
