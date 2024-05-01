059d3802dd016ea32f6314195b1732da10e9a366
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-05-01 15:52:23 +0900

    [Bronze II] Title: 시험 감독, Time: 684 ms, Memory: 145388 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -0,0 +1,18 @@
+import math
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+testPlace = list(map(int, input().split()))
+B, C = map(int, input().split())
+totalTeacher = len(testPlace)
+
+for i in range(N):
+    testPlace[i] -= B
+
+for i in range(N):
+    if testPlace[i] <= 0:
+        continue
+    totalTeacher += math.ceil(testPlace[i]/C)
+
+print(totalTeacher)
