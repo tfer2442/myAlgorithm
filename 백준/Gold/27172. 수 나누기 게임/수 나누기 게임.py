d4ee8e067d0d99a858c572a0cfbf78eba311c0a3
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-05-04 04:41:06 +0900

    [Gold V] Title: 수 나누기 게임, Time: 1692 ms, Memory: 45568 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -2,30 +2,17 @@ import sys
 input = sys.stdin.readline
 
 
-def setDivisor(num):
-    divideNums[num] = {1: True}
-
-    for i in range(2, int(num**(1/2)) + 1):
-        if num % i == 0:
-            divideNums[num][i] = True
-            if (i**2) != num:
-                divideNums[num][num//i] = True
-
-
 n = int(input())
 cards = list(map(int, input().split()))
-vs = {card: 0 for card in cards}
-divideNums = {}
+maxCard = max(cards)
+eratostenes = {card: 0 for card in cards}
 
 for card in cards:
-    setDivisor(card)
+    for i in range(card*2, maxCard+1, card):
+        if i in eratostenes:
+            eratostenes[card] += 1
+            eratostenes[i] -= 1
 
 for card in cards:
-    for divisor in divideNums[card]:
-        if divisor in vs:
-            vs[card] -= 1
-            vs[divisor] += 1
-
-
-for card in cards:
-    print(vs[card], end=' ')
+    print(eratostenes[card], end=' ')
+    
