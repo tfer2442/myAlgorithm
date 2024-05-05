dcbd2edcc7e5264bff6c0428f0ffdc3034af2937
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-05-05 16:54:27 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1720 ms, Memory: 118744 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,54 +1,6 @@
 import sys
+from bisect import bisect_left, bisect_right
 input = sys.stdin.readline
-
-def findSameNumber2(start, left, right, num):
-    while left <= right:
-        mid = (left + right) // 2
-
-        if cards[mid] > num:
-            right = mid - 1
-        elif cards[mid] <= num:
-            left = mid + 1
-
-    return right-start+1
-
-
-def findSameNumber1(start, left, right, num):
-    while left <= right:
-        mid = (left + right) // 2
-
-        if cards[mid] >= num:
-            right = mid - 1
-        elif cards[mid] < num:
-            left = mid + 1
-
-    return start-left+1
-
-def binarySearch(num):
-    left = 0
-    right = n-1
-    cnt = 0
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        if cards[mid] == num:
-            cnt += findSameNumber1(mid, left, mid, num)
-            cnt += findSameNumber2(mid, mid, right, num)
-
-            print(cnt-1, end=' ')
-            return
-
-        if cards[mid] > num:
-            right = mid - 1
-            continue
-
-        if cards[mid] < num:
-            left = mid + 1
-            continue
-
-    print(0, end=' ')
-
 
 n = int(input())
 cards = list(map(int, input().split()))
@@ -57,4 +9,10 @@ m = int(input())
 nums = list(map(int, input().split()))
 
 for num in nums:
-    binarySearch(num)
+    left = bisect_left(cards, num)
+    if left >= n or cards[left] != num:
+        print(0, end=' ')
+        continue
+
+    right = bisect_right(cards, num)
+    print(right-left, end=' ')
