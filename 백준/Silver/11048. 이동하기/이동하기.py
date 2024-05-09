d7015d2e859d49ea93df41d863e794f7bd544b04
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-05-10 02:11:55 +0900

    [Silver II] Title: 이동하기, Time: 804 ms, Memory: 78660 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,31 +1,24 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
-
-def dfs(r, c):
-    if r == n-1 and c == m-1:
-        return maze[r][c]
-
-    for dr, dc in d:
-        nextR, nextC = r + dr, c + dc
-
-        if 0 <= nextR < n and 0 <= nextC < m:
-            if dp[nextR][nextC] == -1:
-                dp[r][c] = max(dp[r][c], dfs(nextR, nextC))
-            else:
-                dp[r][c] = max(dp[r][c], dp[nextR][nextC])
-    dp[r][c] += maze[r][c]
-    return dp[r][c]
 
 
 n, m = map(int, input().split())
 maze = []
-d = [(1, 0), (0, 1), (1, 1)]
 
 for _ in range(n):
     maze.append(list(map(int, input().split())))
 
-dp = [[-1] * m for _ in range(n)]
+dp = [[0] * m for _ in range(n)]
+
 dp[0][0] = maze[0][0]
-print(dfs(0, 0))
+for i in range(1, m):
+    dp[0][i] = dp[0][i-1] + maze[0][i]
+
+for i in range(1, n):
+    dp[i][0] = dp[i-1][0] + maze[i][0]
+
+for i in range(1, n):
+    for j in range(1, m):
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + maze[i][j]
+
+print(dp[n-1][m-1])
