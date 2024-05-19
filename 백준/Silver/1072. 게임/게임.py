da3d9d3ce8eff5745ca411c2b302c8ba11a5f428
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-05-19 17:45:56 +0900

    [Silver III] Title: 게임, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+x, y = map(int, input().split())
+winRate = int(y*100 /x)
+
+if x == y or winRate == 99:
+    print(-1)
+    exit(0)
+
+left = 0
+right = 1_000_000_000_000_000_000_000_000
+answer = 1_000_000_000_000_000_000_000_000
+
+while left <= right:
+    mid = (left + right) // 2
+    curRate = int((y+mid) * 100 / (x+mid) )
+
+    if curRate > winRate:
+        answer = min(answer, mid)
+        right = mid - 1
+    else:
+        left = mid + 1
+
+if answer == 1_000_000_000_000_000_000_000_000:
+    print(-1)
+else:
+    print(answer)
