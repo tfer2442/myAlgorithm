07744dc365b726ae40ec9ec89d5e14ee6ed44508
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-05-21 21:35:48 +0900

    [Gold III] Title: 양팔저울, Time: 92 ms, Memory: 34396 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.py
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -9,25 +9,15 @@ marble = list(map(int, input().split()))
 total = sum(balanceWeight)
 dp = [[0]*(total+1) for _ in range(n+1)]
 
-
 for i in range(1, n+1):
     dp[i] = dp[i-1][:]
     value = balanceWeight[i-1]
-    dp[i][value] += 1
+    dp[i][value] = 1
 
-    for j in range(1, value):
-        prevValue = dp[i-1][j]
-
-        if prevValue >= 1:
-            dp[i][value+j] += 1
-            dp[i][value-j] += 1
-
-    for j in range(value, total+1):
-        prevValue = dp[i-1][j]
-
-        if prevValue >= 1:
-            dp[i][value+j] += 1
-            dp[i][j-value] += 1
+    for j in range(1, total+1):
+        if dp[i-1][j] >= 1:
+            dp[i][value+j] = 1
+            dp[i][abs(value-j)] = 1
 
 for i in range(m):
     if marble[i] <= total and dp[n][marble[i]] >= 1:
