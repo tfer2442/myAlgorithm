6b04c697751f1c99c2da2bcfd84d929025819fa6
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-05-22 17:15:15 +0900

    [Silver I] Title: 경로 찾기, Time: 436 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -12,14 +12,9 @@ for i in range(n):
         if graph[i][j] == 0:
             graph[i][j] = float('inf')
 
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
-
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
             graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
 
 for i in range(n):
