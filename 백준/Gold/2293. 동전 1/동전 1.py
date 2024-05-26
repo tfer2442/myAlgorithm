b745f762b8799729746e9fd45c55f54cd607297c
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-05-26 20:08:42 +0900

    [Gold V] Title: 동전 1, Time: 200 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -8,9 +8,8 @@ dp = [1]+[0]*k
 for _ in range(n):
     coins.append(int(input()))
 
-for i in range(1, n+1):
-    for j in range(1, k+1):
-        if j-coins[i-1] >= 0:
-            dp[j] = dp[j-coins[i-1]] + dp[j]
+for i in range(n):
+    for j in range(coins[i], k+1):
+        dp[j] = dp[j-coins[i]] + dp[j]
 
 print(dp[-1])
