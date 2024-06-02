d03145a7fbc92a94c368626ea6e96b4b29f761a5
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-06-03 01:05:20 +0900

    [Gold IV] Title: 스도쿠, Time: 2692 ms, Memory: 137388 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -12,16 +12,11 @@ for i in range(9):
         if board[i][j] == 0:
             empty.append((i, j))
 
-# 가로
-def check1(row, value):
+
+def check1(row, col, value):
     for i in range(9):
         if board[row][i] == value:
             return False
-    return True
-
-
-def check2(col, value):
-    for i in range(9):
         if board[i][col] == value:
             return False
     return True
@@ -29,7 +24,7 @@ def check2(col, value):
 
 def check3(row, col, value):
     for i in range(row//3 * 3, row//3 * 3 + 3):
-        for j in range(col//3 * 3, col//3 * 3 +3):
+        for j in range(col//3 * 3, col//3 * 3 + 3):
             if board[i][j] == value:
                 return False
     return True
@@ -44,7 +39,7 @@ def dfs(cnt):
     row, col = empty[cnt]
 
     for i in range(1, 10):
-        if check1(row, i) and check2(col, i) and check3(row, col, i):
+        if check1(row, col, i) and check3(row, col, i):
             board[row][col] = i
             dfs(cnt+1)
             board[row][col] = 0
