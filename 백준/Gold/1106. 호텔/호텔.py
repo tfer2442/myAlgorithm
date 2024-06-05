37159b2a248f905ed41dd23cd794377cac98d3df
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-06-06 01:41:30 +0900

    [Gold IV] Title: 호텔, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -1,38 +1,18 @@
 import sys
-from math import ceil
-
 input = sys.stdin.readline
 
-c, n = map(int, input().split())
+C, N = map(int, input().split())
 cities = []
+maxValue = -1
+for _ in range(N):
+    tmp = list(map(int, input().split()))
+    maxValue = max(maxValue, tmp[1])
+    cities.append(tmp)
 
-for _ in range(n):
-    w, p = map(int, input().split())
+dp = [0] + [int(1e9)] * (C+maxValue-1)
 
-    num = ceil(c/p)
-    cnt = 1
-    while True:
-        num -= cnt
+for w, p in cities:
+    for i in range(p, C+maxValue):
+        dp[i] = min(dp[i], dp[i-p]+w)
 
-        if num <= 0:
-            num += cnt
-            cities.append([w*num, p*num])
-            break
-
-        cities.append([w*cnt, p*cnt])
-        cnt *= 2
-
-dp = [[0] * 100001 for _ in range(len(cities)+1)]
-minValue = int(1e9)
-for i in range(1, len(cities)+1):
-    w, p = cities[i-1]
-
-    for j in range(100001):
-        if j < w:
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + p)
-        if dp[i][j] >= c:
-            minValue = min(minValue, j)
-
-print(minValue)
+print(min(dp[C:]))
