d86640468eb7f244724bea2911cedb42f076c392
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-06-10 23:24:35 +0900

    [Gold V] Title: 합분해, Time: 52 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+dp = [[1] * (N+1) for _ in range(K)]
+
+for i in range(1, K):
+    for j in range(1, N+1):
+        dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1_000_000_000
+
+print(dp[K-1][N])
