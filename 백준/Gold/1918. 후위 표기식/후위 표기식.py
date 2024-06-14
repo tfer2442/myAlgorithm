303149b10a7d6b96a8fe28b6b05cda0e3ddbd003
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-06-14 13:20:10 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.py
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -3,37 +3,30 @@ input = sys.stdin.readline
 
 equation = input().strip()
 stack = []
-rank = {'*': 2, '/': 2, '+': 1, '-': 1}
 
 for e in equation:
     if 'A' <= e <= 'Z':
         print(e, end='')
         continue
-    if not stack:
-        stack.append(e)
-        continue
+
     if e == '(':
         stack.append(e)
         continue
-    if e == ')':
-        for i in range(len(stack)-1, -1, -1):
-            if stack[i] == '(':
-                stack.pop()
-                break
-            else:
-                print(stack.pop(), end='')
-        continue
-    for i in range(len(stack) - 1, -1, -1):
-        if stack[-1] == '(':
-            stack.append(e)
-            break
-        if rank[stack[-1]] >= rank[e]:
+
+    if e == '*' or e == '/':
+        while stack and (stack[-1] == '*' or stack[-1] == '/'):
             print(stack.pop(), end='')
-        else:
-            stack.append(e)
-            break
-    else:
         stack.append(e)
+        continue
+    if e == '+' or e == '-':
+        while stack and stack[-1] != '(':
+            print(stack.pop(), end='')
+        stack.append(e)
+        continue
+    if e == ')':
+        while stack and (stack[-1] != '('):
+            print(stack.pop(), end='')
+        stack.pop()
 
 while stack:
     print(stack.pop(), end='')
