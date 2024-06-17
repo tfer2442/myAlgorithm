060d99420e9aa6ca42534d6673c8f7aa5542570e
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-06-17 16:46:31 +0900

    [Silver III] Title: 수리공 항승, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n, l = map(int, input().split())
+l *= 10
+tmp = list(map(int, input().split()))
+tmp.sort()
+parts = list()
+
+for i in range(n):
+    parts.append(tmp[i]*10)
+
+start = parts[0] - 5
+end = start + l
+answer = 1
+
+for i in range(n):
+    if start < parts[i] < end:
+        continue
+    start = parts[i] - 5
+    end = start + l
+    answer += 1
+
+print(answer)
