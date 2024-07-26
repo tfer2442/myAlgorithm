23142a4071d7c0220dfd3a155522ca96d43f08db
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-07-27 00:40:11 +0900

    [Silver III] Title: 배열 복원하기, Time: 1432 ms, Memory: 55724 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	static int H, W, X, Y;
+	static int[][] A;
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] tmp = br.readLine().split(" ");
+		H = Integer.parseInt(tmp[0]);
+		W = Integer.parseInt(tmp[1]);
+		X = Integer.parseInt(tmp[2]);
+		Y = Integer.parseInt(tmp[3]);
+		A = new int[H][W];
+		
+		for (int i = 0; i < H+X; i++) {
+			tmp = br.readLine().split(" ");
+
+			for (int j = 0; j < W; j++) {
+				if (i < H) {
+					A[i][j] = Integer.parseInt(tmp[j]);
+				}
+			}
+		}
+		
+		for (int i = X; i < H; i++) {
+			for (int j = Y; j < W; j++) {
+				A[i][j] -= A[i-X][j-Y];
+			}
+		}
+		
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				System.out.print(A[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
