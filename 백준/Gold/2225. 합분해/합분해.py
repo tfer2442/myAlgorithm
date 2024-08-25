f45fe83d05fd9fbd5d6ec9660f3f6373a3f56114
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-08-25 18:41:45 +0900

    [Gold V] Title: 합분해, Time: 44 ms, Memory: 33164 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -6,6 +6,6 @@ dp = [[1] * (N+1) for _ in range(K)]
 
 for i in range(1, K):
     for j in range(1, N+1):
-        dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1_000_000_000
+        dp[i][j] = (dp[i-1][j] + dp[i][j-1])
 
-print(dp[K-1][N])
+print(dp[K-1][N] % 1_000_000_000)
