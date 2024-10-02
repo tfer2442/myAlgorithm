617638d2a43521747d3fb827fac35624feb01d48
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2024-10-02 10:33:27 +0900

    [D3] Title: 0/1 Knapsack, Time: 145 ms, Memory: 21,500 KB -BaekjoonHub

---
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static int T;
+	public static int N, K;
+	public static int[] dp;
+	public static int[] costs, values;
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			dp = new int[K+1];
+			costs = new int[N+1];
+			values = new int[N+1];
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				costs[i] = Integer.parseInt(st.nextToken());
+				values[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = K; j >= 1; j--) {
+					if (j-costs[i] < 0) {
+						continue;
+					}
+					dp[j] = Math.max(dp[j], dp[j-costs[i]] + values[i]);
+				}
+			}
+			
+			int answer = 0;
+			for (int i = 1; i <= K; i++) {
+				answer = Math.max(answer, dp[i]);
+			}
+			System.out.println("#" + t + " " + answer);
+		}
+	}
+
+}
