8a0ce9bc7f43e96a5924979499b021205b057b9d
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-06 15:24:33 +0900

    [Silver II] Title: 꽃길, Time: 200 ms, Memory: 20148 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.java
+++ 백준/Silver/14620. 꽃길/꽃길.java
@@ -1,76 +1,84 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[][] board; 
-	static boolean[][] visited;
-	static int n;
-	static int[][] d = {{0, 0}, {-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-	static int minValue = Integer.MAX_VALUE;
+	public static int N;
+	public static int[][] board;
+	public static boolean[][] visited;
+	public static int[][] selected;
+	public static int answer;
+	public static int[][] d = {{0, 0}, {0, -1}, {-1, 0}, {0, 1}, {1, 0}};
 	
-	static boolean isPossible(int row, int col) {
-		for (int[] curD : d) {
-			int curRow = curD[0] + row;
-			int curCol = curD[1] + col;
+	public static boolean isAvailable(int r, int c) {
+		
+		for (int[] dd : d) {
+			int nextR = dd[0] + r;
+			int nextC = dd[1] + c;
 			
-			if (visited[curRow][curCol]) {
-				return false;
-			}
+			if (visited[nextR][nextC]) return false;
 		}
+		
 		return true;
 	}
 	
-	
-	static void backtracking(int row, int col, int cnt) {
+	public static void backtracking(int cnt) {
 		if (cnt == 3) {
+			// 고른 것 들의 총 비용 계산하기
 			int total = 0;
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					if (visited[i][j]) {
-						total += board[i][j];
-					}
+			for (int i = 0; i < 3; i++) {
+				for (int[] dd : d) {
+					int r = selected[i][0] + dd[0];
+					int c = selected[i][1] + dd[1];
+					
+					total += board[r][c];
 				}
 			}
-			minValue = Math.min(minValue, total);
+			
+			answer = Math.min(answer, total);
 			return;
 		}
 		
-		for (int i = row; i < n-1; i++) {
-			for (int j = 1; j < n-1; j++) {
-				if (isPossible(i, j)) {
-					visited[i][j] = true;
-					visited[i-1][j] = true;
-					visited[i][j-1] = true;
-					visited[i+1][j] = true;
-					visited[i][j+1] = true;			
-					backtracking(i, j, cnt+1);
-					visited[i][j] = false;
-					visited[i-1][j] = false;
-					visited[i][j-1] = false;
-					visited[i+1][j] = false;
-					visited[i][j+1] = false;
+		for (int i = 1; i < N-1; i++) {
+			for (int j = 1; j < N-1; j++) {
+				if (isAvailable(i, j)) {
+					selected[cnt][0] = i;
+					selected[cnt][1] = j;
+					for (int[] dd : d) {
+						int r = dd[0] + i;
+						int c = dd[1] + j;
+						visited[r][c] = true;
+					}
+					backtracking(cnt+1);
+					for (int[] dd : d) {
+						int r = dd[0] + i;
+						int c = dd[1] + j;
+						visited[r][c] = false;
+					}
 				}
 			}
 		}
 	}
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		board = new int[N][N];
 		
-		n = Integer.parseInt(br.readLine());
-		board = new int[n][n];
-		visited = new boolean[n][n];
-		
-		for (int i = 0; i < n; i++) {
-			String[] str1 = br.readLine().split(" ");
-			for (int j = 0; j < n; j++) {
-				board[i][j] = Integer.parseInt(str1[j]);
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			for (int j = 0; j < N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		backtracking(1, 1, 0);
-		System.out.println(minValue);
+		visited = new boolean[N][N];
+		selected = new int[3][2];
+		answer = Integer.MAX_VALUE;
+		backtracking(0);
+		System.out.println(answer);
 	}
+
 }
