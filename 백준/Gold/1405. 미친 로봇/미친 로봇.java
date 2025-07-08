31e83dec447e37d96458c10d8bf2204fa3227254
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-08 16:32:24 +0900

    [Gold IV] Title: 미친 로봇, Time: 156 ms, Memory: 14888 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.java
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.java
@@ -1,51 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.text.DecimalFormat;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int n;
-	static int[][] board;
-	static boolean[][] visited;
-	static int[][] d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-	static int[] p = new int[4];
-	static double result = 0;
+	public static int N;
+	public static int[] rate;
+	public static int[][] d = {{0, 1}, {0, -1}, {1, 0}, {-1 ,0}};
+	public static boolean[][] visited;
+	public static double answer = 0;
 	
-	public static void dfs(int row, int col, int depth, double total) {
-		if (depth == n) {
-			result += total;
+	public static void dfs(int depth, int r, int c, double curRate) {
+		if (depth >= N) {
+			answer += curRate;
 			return;
 		}
-		
+				
 		for (int i = 0; i < 4; i++) {
-			int nextRow = d[i][0] + row;
-			int nextCol = d[i][1] + col;
+			if (rate[i] == 0) continue;
 			
-			if (visited[nextRow][nextCol]) continue;
-			visited[nextRow][nextCol] = true;
-			dfs(nextRow, nextCol, depth+1, total*(p[i]/100.0));
-			visited[nextRow][nextCol] = false;
+			int nextR = r + d[i][0];
+			int nextC = c + d[i][1];
+			
+			if (visited[nextR][nextC]) continue;
+			visited[nextR][nextC] = true;
+			dfs(depth+1, nextR, nextC, curRate * (rate[i]/100.0));
+			visited[nextR][nextC] = false;
 		}
 	}
-	
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		n = Integer.parseInt(st.nextToken());
-		board = new int[(n+1)*2+1][(n+1)*2+1];
-		visited = new boolean[(n+1)*2+1][(n+1)*2+1];
+		N = Integer.parseInt(st.nextToken());
+		rate = new int[4];
 		
 		for (int i = 0; i < 4; i++) {
-			p[i] = Integer.parseInt(st.nextToken());
+			rate[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		visited[n+1][n+1] = true;
-		dfs(n+1, n+1, 0, 1);
-		System.out.println(result);
+		visited = new boolean[2*N+1][2*N+1];
+		visited[N][N] = true;
+		dfs(0, N, N, 1);
+		System.out.println(answer);
 	}
 }
