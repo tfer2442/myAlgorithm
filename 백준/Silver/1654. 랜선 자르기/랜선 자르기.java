531b44e87565c6a379c4f3bb8c6ac854cb47d536
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-09 10:24:55 +0900

    [Silver II] Title: 랜선 자르기, Time: 164 ms, Memory: 17188 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,53 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static int K, N;
-	public static int[] arr;
-	
-	static long calculate(long mid) {
-		long cnt = 0;
-		
-		for (int i = 0; i < K; i++) {
-			cnt += arr[i] / mid;
-		}
-		
-		return cnt;
-	}
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] tmp = br.readLine().split(" ");
-		K = Integer.parseInt(tmp[0]);
-		N = Integer.parseInt(tmp[1]);
-		arr = new int[K];
+    public static int K, N;
+    public static int[] nums;
+    
+    public static int calculate(long mid) {
+        int cnt = 0;
+        
+        for (int i = 0; i < K; i++) {
+            cnt += nums[i] / mid;
+        }
+        
+        return cnt;
+    }
 
-		for (int i = 0; i < K; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		long left = 1;
-		long right = 0;
-		
-		for (int i = 0; i < K; i++) {
-			right = Math.max(right, arr[i]);
-		}
-		
-		long mid = 0;
-		long answer = 0;
-		
-		while (left <= right) {
-			mid = (left+right)/2;
-			long result = calculate(mid);
-			
-			if (result < N) {
-				right = mid - 1;
-			} else {
-				left = mid + 1;
-				answer = Math.max(answer, mid);
-			}
-		}
-		System.out.println(answer);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        
+        nums = new int[K];
+        
+        long left = 1;
+        long right = 0;  // 최대값으로 설정할 예정
+        
+        for (int i = 0; i < K; i++) {
+            int tmp = Integer.parseInt(br.readLine());
+            nums[i] = tmp;
+            right = Math.max(right, tmp);  // 입력된 랜선 중 최대값
+        }
+
+        long answer = 1;
+        while (left <= right) {
+            long mid = (left + right) / 2;  // 정수 나눗셈
+            int tmp = calculate(mid);
+                        
+            if (tmp >= N) {
+                answer = Math.max(answer, mid);
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        System.out.println(answer);
+    }
 }
