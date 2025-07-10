65253ec1032c2664e7cee305723dc38ca88530fe
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-10 15:03:14 +0900

    [Gold IV] Title: 합이 0, Time: 2820 ms, Memory: 18916 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -1,82 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	public static int N;
-	public static int[] arr;
+	public static int[] scores;
 	
-	static int lowerBS(int left, int twoSum) {
-		int right = N-1;
-		int result = -1;
-		
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			
-			if (arr[mid] + twoSum > 0) {
-				right = mid - 1;
-			} else if (arr[mid] + twoSum < 0) {
-				left = mid + 1;
-			} else if (arr[mid] + twoSum == 0){
-				right = mid - 1;
-				result = mid;
-			}
-		}
-		
-		return result;
-	}
-	
-	static int upperBS(int left, int twoSum) {
-		int right = N-1;
-		int result = -1;
-		
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			
-			if (arr[mid] + twoSum > 0) {
-				right = mid - 1;
-			} else if (arr[mid] + twoSum < 0) {
-				left = mid + 1;
-			} else if (arr[mid] + twoSum == 0){
-				left = mid + 1;
-				result = mid;
-			}
-		}
-		
-		return result;
-	}
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		String[] tmp = br.readLine().split(" ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		scores = new int[N];
+		
+		for (int i =0; i < N; i++) {
+			scores[i] = Integer.parseInt(st.nextToken());
+		}
 		
 		if (N <= 2) {
 			System.out.println(0);
-			System.exit(0);
+			return;
 		}
 		
-		arr = new int[N];
-		
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(tmp[i]);
-		}
-		
-		Arrays.sort(arr);
-		
+		Arrays.sort(scores);
 		long answer = 0;
+		
 		for (int i = 0; i < N-2; i++) {
 			for (int j = i+1; j < N-1; j++) {
-				int l = lowerBS(j+1, arr[i]+arr[j]);
-				int r = upperBS(j+1, arr[i]+arr[j]);
-				if (l == -1) {
-					continue;
+				int left = j + 1;
+				int right = N - 1;
+				int sum = scores[i] + scores[j];
+				int target = -1 * sum;
+				int leftIdx = N;
+				
+				// target 맨 왼쪽 인덱스 찾기
+				while (left <= right) {
+					int mid = (left+right)/2;
+					
+					if (scores[mid] > target) {
+						right = mid - 1;
+					} else if (scores[mid] < target) {
+						left = mid + 1;
+					} else if (scores[mid] == target) {
+						leftIdx = mid;
+						right = mid - 1;
+					}
 				}
-				answer += r-l+1;
+								
+				// target 값이 없다면 반복문 넘어가기
+				if (leftIdx == N) continue;
+				
+				left = j + 1;
+				right = N - 1;
+				int rightIdx = -1;
+				// target 맨 오른쪽 인덱스 찾기
+				while (left <= right) {
+					int mid = (left+right)/2;
+					
+					if (scores[mid] > target) {
+						right = mid - 1;
+					} else if (scores[mid] < target) {
+						left = mid + 1;
+					} else if (scores[mid] == target) {
+						rightIdx = mid;
+						left = mid + 1;
+					}
+				}
+				
+				answer += (rightIdx - leftIdx) + 1;
 			}
 		}
 		System.out.println(answer);
-		
-	
 	}
 }
