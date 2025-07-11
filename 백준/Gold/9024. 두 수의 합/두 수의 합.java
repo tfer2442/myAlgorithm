f45d0da2b18b722390e21c0a2ae04cee4070eb90
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-11 14:49:22 +0900

    [Gold V] Title: 두 수의 합, Time: 1920 ms, Memory: 228288 KB -BaekjoonHub

--- 백준/Gold/9024. 두 수의 합/두 수의 합.java
+++ 백준/Gold/9024. 두 수의 합/두 수의 합.java
@@ -1,60 +1,61 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static int n, k;
-	public static int[] arr;
+	public static int T;
+	public static int N, K;
+	public static int[] nums;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		T = Integer.parseInt(br.readLine());
 		
-		for (int t = 1; t <= T; t++) {
-			String[] tmp = br.readLine().split(" ");
-			n = Integer.parseInt(tmp[0]);
-			k = Integer.parseInt(tmp[1]);
-			tmp = br.readLine().split(" ");
-			arr = new int[n];
+		for (int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
 			
-			for (int i = 0; i < n; i++) {
-				arr[i] = Integer.parseInt(tmp[i]);
+			nums = new int[N];
+			st = new StringTokenizer(br.readLine());
+			
+			for (int i = 0; i < N; i++) {
+				nums[i] = Integer.parseInt(st.nextToken());
 			}
-			Arrays.sort(arr);
 			
-			int closeCnt = 0;
-			long closeValue = Integer.MAX_VALUE; 
-
-			for (int i = 0; i < n-1; i++) {
-				int left = i+1;
-				int right = n-1;
-						
+			Arrays.sort(nums);
+			
+			int closedValue = 1_000_000_000;
+			int answer = 0;
+			
+			for (int i = 0 ; i < N-1; i++) {
+				int left = i + 1;
+				int right = N - 1;
+				
 				while (left <= right) {
-					int mid = (left + right) / 2;
-					long result = arr[mid] + arr[i];
+					int mid = (left+right)/2;
 					
-					if (closeValue > Math.abs(result-k)) {
-						closeValue = Math.abs(result-k);
-						closeCnt = 1;
-					} else if (closeValue == Math.abs(result-k)) {
-						closeCnt += 1;
+					int value = nums[i] + nums[mid];
+					int tmp = Math.abs(K-value);
+					
+					if (closedValue > tmp) {
+						closedValue = tmp;
+						answer = 1;
+					} else if (closedValue == tmp){
+						answer++;
 					}
-					
-					if (result < k) {
+
+					if (value < K) {
 						left = mid + 1;
-						
-					} else if (result > k) {
+					} else if (value > K) {
 						right = mid - 1;
-					} else if (result == k) {
+					} else {
 						break;
 					}
 				}
 			}
-			System.out.println(closeCnt);
+			
+			System.out.println(answer);
 		}
-
 	}
-
 }
