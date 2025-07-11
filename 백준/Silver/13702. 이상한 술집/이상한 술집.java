428e5c8b7ff43483fba205203f5ab1683acd6ec8
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-11 14:18:49 +0900

    [Silver II] Title: 이상한 술집, Time: 156 ms, Memory: 17300 KB -BaekjoonHub

--- 백준/Silver/13702. 이상한 술집/이상한 술집.java
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.java
@@ -1,51 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	public static int N, K;
-	public static int[] arr;
+	public static int[] nums;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] tmp = br.readLine().split(" ");
-		N = Integer.parseInt(tmp[0]);
-		K = Integer.parseInt(tmp[1]);
-		arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		long right = -1;
-		long total = 0;
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			total += arr[i];
-			right = Math.max(right, arr[i]);
-		}
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 		
-		if (total < N) {
-			System.out.println(0);
-			System.exit(0);
-		}
+		nums = new int[N];
 		
 		long left = 1;
-		long maxValue = 0;
+		long right = 0;
+		
+		for (int i = 0 ; i < N; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+			right = Math.max(right, nums[i]);
+		}
+		
+		long answer = 0;
 		
 		while (left <= right) {
-			long mid = (left+right)/2;
-			long cnt = 0;
+			long mid = (left + right) / 2;
+			
+			int cnt = 0;
 			
 			for (int i = 0; i < N; i++) {
-				cnt += arr[i] / mid;
+				cnt += nums[i] / mid;
 			}
 			
-			if (K <= cnt) {
+			if (cnt >= K) {
+				answer = mid;
 				left = mid + 1;
-				maxValue = Math.max(maxValue, mid);
-			} else {
+			} else if (cnt < K) {
 				right = mid - 1;
 			}
 		}
-		System.out.println(maxValue);
+		
+		System.out.println(answer);
 	}
 
 }
