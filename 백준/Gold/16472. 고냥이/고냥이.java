a524190b14585a129b06e0e78e6d965d77966569
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-14 21:36:32 +0900

    [Gold IV] Title: 고냥이, Time: 224 ms, Memory: 18912 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,46 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	public static int N;
-	public static String str1;
-	public static HashMap<Character, Integer> hash = new HashMap<>();;
+	public static String str;
+	public static HashMap<Character, Integer> alphabetCount;
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		N = Integer.parseInt(br.readLine());
-		str1 = br.readLine();
-
+		str = br.readLine();
+		
+		// left, right 필요
+		// 한 칸씩 보면서(알파벳 초과하는지 확인), 만약에 초과하지 않는다면 right ++
+		// 만약에 초과한다면? 새로운 알파벳이 들어온 것이므로, left를 ++ 하며, 알파벳의 종류 갯수를 맞춰줘야 함
+		
+		alphabetCount = new HashMap<>();
+		
 		int left = 0;
 		int right = 0;
-		int cnt = 0;
-		int maxValue = -1;
+		int len = 0;
+		int answer = 0;
 		
-		while (right < str1.length() && left <= right) {
-
-			if (hash.get(str1.charAt(right)) != null || hash.size() < N) {
-				if (hash.get(str1.charAt(right)) == null) {
-					hash.put(str1.charAt(right), 1);
-				} else {
-					hash.put(str1.charAt(right), hash.get(str1.charAt(right))+1);
-				}
-				cnt++;
+		while (left < str.length() && right < str.length()) {
+			Character target = str.charAt(right);
+			
+			if (alphabetCount.containsKey(target)) {
+				len++;
 				right++;
-			} else {
-				if (hash.get(str1.charAt(left)) >= 2) {
-					hash.put(str1.charAt(left), hash.get(str1.charAt(left))-1);
-				} else {
-					hash.remove(str1.charAt(left));
-				}
-				left++;
-				cnt--;
+				
+				answer = Math.max(len, answer);
+				alphabetCount.put(target, alphabetCount.get(target)+1);
+				continue;
 			}
-			maxValue = Math.max(maxValue, cnt);
+			
+			if (alphabetCount.size() < N) {
+				len++;
+				right++;
+				
+				answer = Math.max(len, answer);
+				alphabetCount.put(target, 1);
+				continue;
+			}
+			
+			while (left <= right) {
+				if (alphabetCount.size() < N) {
+					len++;
+					right++;
+					alphabetCount.put(target, 1);
+					break;
+				}
+
+				if (alphabetCount.get(str.charAt(left)) >= 2) {
+					alphabetCount.put(str.charAt(left), alphabetCount.get(str.charAt(left))-1);  // 수정: str.charAt(left)-1을 alphabetCount.get(str.charAt(left))-1로 변경
+				} else {
+					alphabetCount.remove(str.charAt(left));
+				}
+				
+				len--;
+				left++;
+			}
 		}
-		
-		System.out.println(maxValue);
+		System.out.println(answer);
 	}
 }
