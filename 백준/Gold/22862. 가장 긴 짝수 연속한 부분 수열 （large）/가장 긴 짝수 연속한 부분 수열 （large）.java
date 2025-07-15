518c815df8b66b250f51377e9282475b363a66a0
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-15 22:25:22 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 436 ms, Memory: 91224 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -1,22 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	public static int n, k;
 	public static int[] arr;
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] tmp = br.readLine().split(" ");
-		n = Integer.parseInt(tmp[0]);
-		k = Integer.parseInt(tmp[1]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
 		arr = new int[n];
-		tmp = br.readLine().split(" ");
+		
+		st = new StringTokenizer(br.readLine());
 		
 		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(tmp[i]);
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		int cnt = 0;
@@ -27,18 +28,16 @@ public class Main {
 			if (arr[right] % 2 == 1) {
 				cnt++;
 				
-				while (k < cnt) {
+				while (cnt > k) {
 					if (arr[left] % 2 == 1) {
-						left++;
 						cnt--;
-					} else {
-						left++;
 					}
+					
+					left++;
 				}
 			}
 			answer = Math.max(answer, right-left-cnt+1);
 		}
 		System.out.println(answer);
 	}
-
 }
