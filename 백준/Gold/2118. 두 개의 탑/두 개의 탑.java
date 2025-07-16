42980c7a75df44ec7f1fd8287259af189a1b5305
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-16 13:31:08 +0900

    [Gold V] Title: 두 개의 탑, Time: 172 ms, Memory: 19796 KB -BaekjoonHub

--- 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
@@ -1,41 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static int n;
-	public static int[] arr;
+	public static int N, total;
+	public static int[] nums;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		int total = 0;
-		arr = new int[n];
+		N = Integer.parseInt(br.readLine());
+		nums = new int[N];
 		
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			total += arr[i];
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+			total += nums[i];
 		}
 		
 		int left = 0;
-		int sum = arr[0];
-		int answer = Math.min(total-sum, sum);
+		int right = 1;
+		int d = nums[0];
+		int answer = 0;
 		
-		for (int right = 1; right < n; right++) {
-			sum += arr[right];
-			int a = Math.min(total-sum, sum);
-			answer = Math.max(answer, a);
-		
-			while (sum > total-sum) {
-				sum -= arr[left];
+		while (left <= right && right < N) {
+			if (total - d > d) {
+				d += nums[right];
+				right++;
+			} else {
+				d -= nums[left];
 				left++;
-				a = Math.min(total-sum, sum);
-				answer = Math.max(answer, a);
 			}
+			
+			answer = Math.max(answer, Math.min(d, total-d));
 		}
 		
 		System.out.println(answer);
 	}
-
 }
