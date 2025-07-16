f8d09d4b7a5a72d7d8303c5ee75e40fb7404d313
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-16 19:38:21 +0900

    [Silver I] Title: 효율적인 해킹, Time: 9008 ms, Memory: 309656 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,75 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
+
 
 public class Main {
-	public static int N, M;
-	public static ArrayList<ArrayList<Integer>> graph;
-	public static boolean[] visited;
-	public static int[] result;
-	
-	public static void bfs(int cnt) {
-		int answer = 0;
-		ArrayDeque<Integer> dq = new ArrayDeque<>();
-		visited = new boolean[N+1];
-		visited[cnt] = true;
-		dq.add(cnt);
-		
-		while (!dq.isEmpty()) {
-			int cur = dq.poll();
-			
-			for (int next : graph.get(cur)) {
-				if (visited[next]) continue;
-				visited[next] = true;
-				dq.add(next);
-				answer++;
-			}
-		}
-		
-		result[cnt] = answer;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		result = new int[N+1];
-		graph = new ArrayList<>();
-		
-		for (int i = 0; i <= N; i++) {
-			graph.add(new ArrayList<>());
-		}
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			graph.get(b).add(a);
-		}
-		
-		for (int i = 1; i <= N; i++) {
-			bfs(i);
-		}
-		
-		int max = 0;
-		for (int i = 1; i <= N; i++) {
-			if (max < result[i]) {
-				max = result[i];
-			}
-		}
-		
-		for (int i = 1; i <= N; i++) {
-			if (max == result[i]) {
-				System.out.print(i + " ");
-			}
-		}
-	}
+   public static int N, M;
+   public static ArrayList<Integer>[] graph;
+   public static boolean[] visited;
+   public static int[] nums;
+   
+   
+   public static void main(String[] args) throws Exception {
+   	// TODO Auto-generated method stub
+   	BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 65536);
+   	StringTokenizer st = new StringTokenizer(br.readLine());
+   	
+   	N = Integer.parseInt(st.nextToken());
+   	M = Integer.parseInt(st.nextToken());
+   	
+   	graph = new ArrayList[N+1];
+   	nums = new int[N+1];
+   	
+   	for (int i = 1; i <= N; i++) {
+   		graph[i] = new ArrayList<>();
+   	}
+   	
+   	for (int i = 0; i < M; i++) {
+   		st = new StringTokenizer(br.readLine());
+   		int a = Integer.parseInt(st.nextToken());
+   		int b = Integer.parseInt(st.nextToken());
+   		
+   		graph[b].add(a);
+   	}
+   	
+   	for (int i = 1; i <= N; i++) {
+   		visited = new boolean[N+1];
+
+   		Queue<Integer> queue = new LinkedList<>();
+   		
+   		visited[i] = true;
+   		queue.offer(i);
+   		int cnt = 1;
+   		
+   		while (!queue.isEmpty()) {
+   			int cur = queue.poll();
+   			
+   			for (int next : graph[cur]) {
+   				if (visited[next]) continue;
+   				visited[next] = true;
+   				queue.offer(next);
+   				cnt++;
+   			}
+   		}
+   		
+   		nums[i] = cnt;
+   	}
+   	
+   	int maxValue = -1;
+   	
+   	for (int i = 1; i <= N; i++) {
+   		maxValue = Math.max(maxValue, nums[i]);
+   	}
+   	
+   	StringBuilder sb = new StringBuilder();
+   	
+   	for (int i = 1; i <= N; i++) {
+   		if (nums[i] == maxValue) {
+   			sb.append(i).append(" ");
+   		}
+   	}
+   	
+   	System.out.println(sb);
+   }
 }
