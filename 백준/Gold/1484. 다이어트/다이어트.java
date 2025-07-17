8991b44c448aefcfd6bed4296cdb306ec43fa41d
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-17 18:40:53 +0900

    [Gold V] Title: 다이어트, Time: 112 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Gold/1484. 다이어트/다이어트.java
+++ 백준/Gold/1484. 다이어트/다이어트.java
@@ -1,61 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+import java.io.*;
 
+/*
+ * 현재 가능한 몸무게를 오름차순으로 출력하는 문제
+ * 가능한 몸무게 없으면 -1, 자연수로 떨어지지 않으면 제외
+ * G = (현재몸무게)^2 - (기억하고있던 몸무게)^2
+ * G는 자연수, 몸무게도 자연수
+ */
 public class Main {
-	public static int G;
-	public static ArrayList<Integer> arr;
-	public static ArrayList<Integer> answer;
- 	
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		G = Integer.parseInt(br.readLine());
-		arr = new ArrayList<>();
-		answer = new ArrayList<>();
-		arr.add(1);
-		arr.add(4);
-		int cur = 3;
-		while (true) {
-			if (arr.get(arr.size()-1) - arr.get(arr.size()-2) > G) {
-				break;
-			}
-			
-			arr.add(cur*cur);
-			cur++;
-		}
-		
-		for (int i = 0; i < arr.size()-1; i++) {
-			int left = i+1;
-			int right = arr.size()-1;
-			int target = G + arr.get(i);
-			
-			while (left <= right) {
-				int mid = (left+right) / 2;
-				
-				if (arr.get(mid) == target) {
-					answer.add(mid+1);
-					break;
-				} else if (arr.get(mid) < target) {
-					left = mid + 1;
-				} else {
-					right = mid - 1;
-				}
-			}
-		}
-		
-		if (answer.size() == 0) {
-			System.out.println(-1);
-		} else {
-			Collections.sort(answer);
-			
-			for (int i : answer) {
-				System.out.println(i);
-			}
-		}
-
-	}
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int G = Integer.parseInt(br.readLine());
+        
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        // 투 포인터 알고리즘
+        // past: 기억하고 있던 몸무게, current: 현재 몸무게
+        int past = 1;
+        int current = 2;
+        
+        // current^2 - past^2 <= G를 만족하는 동안 반복
+        while (true) {
+            long diff = (long)current * current - (long)past * past;
+            
+            // 차이가 G보다 크고, past를 더 이상 증가시켜도 답이 없을 때 종료
+            if (diff > G && past == current - 1) {
+                break;
+            }
+            
+            if (diff == G) {
+                answer.add(current);
+                past++;
+                current++;
+            } else if (diff > G) {
+                past++;
+            } else {
+                current++;
+            }
+        }
+        
+        // 결과 출력
+        if (answer.size() == 0) {
+            System.out.println(-1);
+        } else {
+            for (int num : answer) {
+                System.out.println(num);
+            }
+        }
+    }
 }
