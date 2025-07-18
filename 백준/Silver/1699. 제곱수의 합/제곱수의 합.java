3c68b5ce01b4b61e21e326fc884396c636ae2d7f
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-07-18 11:44:03 +0900

    [Silver II] Title: 제곱수의 합, Time: 144 ms, Memory: 14872 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -1,31 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
+
+/* 각 숫자들의 항의 최소 개수를 배열에 저장하고, 이후 배열에서 활용
+ * 현재 위치의 숫자에 -1, -4, -9 ... 제곱수들을 빼보며 최소값 + 1이 현재 배열의 값
+ * 루트로 떨어진다면 항의 최소 개수는 1
+ * 
+ */
 public class Main {
-	public static int n;
+	public static int N;
 	public static int[] dp;
-
-	public static void main(String[] args) throws IOException{
+	
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		dp = new int[n+1];
+		N = Integer.parseInt(br.readLine());
+		dp = new int[N+1];
 		dp[1] = 1;
 		
-		for (int i = 2; i <= n; i++) {
-			if (Math.sqrt(i)%1 == 0) {
-				dp[i] = 1;
-				continue;
+		for (int i = 2; i <= N; i++) {
+			int target = Integer.MAX_VALUE;
+			
+			for (int j = 1; i-j*j >= 0 ; j++) {
+				target = Math.min(dp[i-j*j], target);
 			}
-			for (int j = 1; j <= Math.sqrt(i); j++) {
-				if (dp[i] == 0 || dp[i] > dp[i-j*j] + dp[j*j]) {
-					dp[i] = dp[i-j*j] + dp[j*j];
-				}
-			}
+			dp[i] = target+1;
 		}
 		
-		System.out.println(dp[n]);
+		System.out.println(dp[N]);
 	}
-
 }
