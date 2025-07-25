925243b76547e4b98cdab2ddda57cb1cbeb27889
tfer2442 <tfer2442@naver.com>
2025-07-25 20:09:46 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 412 ms, Memory: 48584 KB -BaekjoonHub

--- 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.java
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.java
@@ -1,22 +1,38 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.io.*;
 
+/*
+ * 1. 순서를 조정한다는 것에 집중 X
+ * 2. 연결 그래프에서 s와 t의 최소 거리 구하는 문제
+ * 3. 다익스트라 알고리즘으로 해결 가능
+ */
 public class Main {
-	public static int n, m;
+	public static int N, M, S, T;
 	public static ArrayList<int[]>[] graph;
 	public static int[] distance;
-	public static PriorityQueue<Node> pq;
 	
-	public static class Node {
-		int v;
-		int value;
+	public static void dijkstra() {
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+			return o1[1] - o2[1];
+		});
 		
-		public Node(int v, int value) {
-			this.v = v;
-			this.value = value;
+		pq.add(new int[] {S, 0});
+		
+		while (!pq.isEmpty()) { 
+			int[] node = pq.poll();
+			int cur = node[0];
+			int value = node[1];
+			
+			if (distance[cur] < value) continue;
+			for (int[] nextNode : graph[cur]) {
+				int next = nextNode[0];
+				int nextValue = nextNode[1];
+				
+				if (distance[cur] + nextValue < distance[next]) {
+					distance[next] = distance[cur] + nextValue;
+					pq.add(new int[] {next, distance[next]});
+				} 
+			}
 		}
 	}
 	
@@ -24,53 +40,37 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		graph = new ArrayList[n+1];
 		
-		for (int i = 1; i <= n; i++) {
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		graph = new ArrayList[N+1];
+		distance = new int[N+1];
+		
+		for (int i = 1; i <= N; i++) {
 			graph[i] = new ArrayList<>();
+			distance[i] = 1_000_000_000;
 		}
 		
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			int value = Integer.parseInt(st.nextToken());
-			graph[s].add(new int[] {e, value});
-			graph[e].add(new int[] {s, value});
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			graph[a].add(new int[] {b, c});
+			graph[b].add(new int[] {a, c});
 		}
-		
-		distance = new int[n+1];
-		pq = new PriorityQueue<>((o1, o2) -> o1.value-o2.value);
 		
 		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
 		
-		for (int i = 1; i <= n; i++) {
-			distance[i] = Integer.MAX_VALUE;
-		}
-		distance[start] = 0;
-		pq.add(new Node(start, 0));
+		distance[S] = 0;
 		
-		while (!pq.isEmpty()) {
-			Node node = pq.poll();
-			
-			if (distance[node.v] < node.value) continue;
-			
-			for (int[] nextNode : graph[node.v]) {
-				int nextV = nextNode[0];
-				int nextValue = nextNode[1];
-				
-				if (distance[nextV] > distance[node.v] + nextValue) {
-					distance[nextV] = distance[node.v] + nextValue;
-					pq.add(new Node(nextV, distance[nextV]));
-				}
- 			}
-		}
+		dijkstra();
 		
-		System.out.println(distance[end]);
+		System.out.println(distance[T]);
 	}
-
 }
