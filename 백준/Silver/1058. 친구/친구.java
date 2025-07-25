454bc29f14897c14ada2089928982623764c4d00
tfer2442 <tfer2442@naver.com>
2025-07-25 20:15:55 +0900

    [Silver II] Title: 친구, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.java
+++ 백준/Silver/1058. 친구/친구.java
@@ -1,59 +1,66 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
+/*
+ * 거리가 2이내의 친구가 가장 많은 사람이 정답
+ * 플로이드-와샬을 이용해 모든 사람들 간의 최단거리를 구하면 된다.
+ * O(N^3), 1 <= N <= 50 : 가능
+ */
 public class Main {
 	public static int N;
 	public static int[][] board;
-	
+
+	public static void floydWarshall() {
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				if (board[i][k] == Integer.MAX_VALUE) continue;
+				
+				for (int j = 0; j < N; j++) {
+					if (board[k][j] == Integer.MAX_VALUE) continue;
+					
+					board[i][j] = Math.min(board[i][j], board[i][k] + board[k][j]);
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
+		
 		board = new int[N][N];
 		
 		for (int i = 0; i < N; i++) {
 			String tmp = br.readLine();
+			
 			for (int j = 0; j < N; j++) {
-				board[i][j] = tmp.charAt(j) - 'N';
-			}
-		}
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (i == j) {
-					board[i][j] = 0;
-					continue;
-				}
-				
-				if (board[i][j] == 0) {
+				if (i == j) continue;
+				if (tmp.charAt(j) == 'N') {
 					board[i][j] = Integer.MAX_VALUE;
 				} else {
 					board[i][j] = 1;
 				}
+				
 			}
 		}
 		
-		for (int k = 0; k < N; k++) {
-			for (int i = 0; i < N; i++) {
-				if (board[i][k] == Integer.MAX_VALUE) continue;
-				for (int j = 0; j < N; j++) {
-					if (board[k][j] == Integer.MAX_VALUE) continue;
-					board[i][j] = Math.min(board[i][j], board[i][k]+board[k][j]);
-				}
-			}
-		}
+		floydWarshall();
 		
 		int answer = 0;
+		
 		for (int i = 0; i < N; i++) {
-			int twoFriend = 0;
+			int cnt = 0;
 			
 			for (int j = 0; j < N; j++) {
-				if (i == j) continue;
-				if (board[i][j] <= 2) twoFriend++;
+				if (board[i][j] <= 2) {
+					cnt++;
+				}
 			}
-			answer = Math.max(answer, twoFriend);
+			
+			answer = Math.max(answer, cnt-1);
 		}
+		
 		System.out.println(answer);
 	}
-
 }
