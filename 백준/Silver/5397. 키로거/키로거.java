5323b2c7cad099dfde92e9fb5c568b1b06a73a57
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-08-22 22:07:21 +0900

    [Silver II] Title: 키로거, Time: 696 ms, Memory: 90384 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -2,6 +2,11 @@ import java.util.*;
 import java.io.*;
 
 /*
+ * 다시 풀어보기!!
+ * 
+ * iterator와 StringBuilder에 대해서 잘 알 것.
+ * stack으로도 풀 수 있음.
+ * 
  * 알파벳 대문자, 소문자, 숫자, 백스페이스(-), 화살표(<, >)
  * 링크드리스트 문제
  */
@@ -16,40 +21,45 @@ public class Main {
 		for (int t = 0; t < T; t++) {
 			String str = br.readLine();
 			
-			LinkedList<Character> password = new LinkedList<>();
-			ListIterator<Character> it = password.listIterator();
+			// 커서 왼쪽 dq1, 커서 오른쪽 dq2
+			ArrayDeque<Character> dq1 = new ArrayDeque<>();
+			ArrayDeque<Character> dq2 = new ArrayDeque<>();
 			
 			for (int i = 0; i < str.length(); i++) {
 				char c = str.charAt(i);
 				
 				if (c == '<') {
-					if (it.hasPrevious()) {
-						it.previous();
+					if (!dq1.isEmpty()) {
+						dq2.push(dq1.pop());						
 					}
 					continue;
 				}
 				
 				if (c == '>') {
-					if (it.hasNext()) {
-						it.next();
+					if (!dq2.isEmpty()) {
+						dq1.push(dq2.pop());
 					}
 					continue;
 				}
 				
 				if (c == '-') {
-					if (it.hasPrevious()) {
-						it.previous();
-						it.remove();
+					if (!dq1.isEmpty()) {
+						dq1.pop();
 					}
 					continue;
 				}
 				
-				it.add(c);
+				dq1.push(c);
 			}
 			
 			StringBuilder sb = new StringBuilder();
 			
-			for (char c : password) {
+			for (char c : dq1) {
+				sb.append(c);
+			}
+			sb.reverse();
+			
+			for (char c : dq2) {
 				sb.append(c);
 			}
 			
