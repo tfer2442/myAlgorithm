b59bf8bde297657e3e0f30f7c396ca7caf971c12
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-09-30 23:29:59 +0900

    [Gold IV] Title: N-Queen, Time: 5864 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,49 +1,47 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
-	public static int N;
-	public static int[] chess;
-	public static int answer;
+	public static int N, answer;
+	public static int[] board;
 	
-	public static boolean isAvailable(int cur, int col) {
-		for (int i = 0; i < cur; i++) {
-			// 기울기가 1이거나 -1이라면 놓을 수 없음
-			if (Math.abs(cur-i) == Math.abs(col-chess[i])) {
+	public static boolean isPlaceable(int depth, int num) {
+		for (int i = 0; i < depth; i++) {
+			if (Math.abs(i-depth) == Math.abs(board[i]-num)) {
 				return false;
 			}
 			
-			// 같은 열에 둘 수 없음
-			if (chess[i] == col) {
+			if (board[i] == num) {
 				return false;
 			}
 		}
+		
 		return true;
 	}
 	
-	// 현재 줄을 입력으로 받아야 함
-	public static void backTracking(int cur) {
-		if (cur >= N) {
+	public static void nQueen(int depth) {
+		if (depth == N) {
 			answer++;
 			return;
 		}
 		
 		for (int i = 0; i < N; i++) {
-			// 현재 줄에서 i번째 열에 퀸을 두어도 되는지 확인
-			if (isAvailable(cur, i)) {
-				chess[cur] = i;
-				backTracking(cur+1);
+			if (isPlaceable(depth, i)) {
+				board[depth] = i;
+				nQueen(depth+1);
 			}
 		}
 	}
 	
-    public static void main(String[] args) throws Exception {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	N = Integer.parseInt(br.readLine());
-    	chess = new int[N];
-    	
-    	backTracking(0);
-    	System.out.println(answer);
-    }
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		board = new int[N];
+		
+		nQueen(0);
+		
+		System.out.println(answer);
+	}
 }
