cfe0a00415f4c3dd77a4866f4a62211e02ced69c
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-10-05 17:54:21 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 116 ms, Memory: 15544 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -4,56 +4,55 @@ import java.io.*;
 public class Main {
 	public static int N;
 	public static int[] scv;
+	public static int[][][] dp;
+	public static int[][] attack = {{9, 3, 1}, {9, 1, 3}, {3, 9, 1}, {3, 1, 9}, {1, 9, 3}, {1, 3, 9}};
 	public static int answer = Integer.MAX_VALUE;
-	public static int[][][] visited = new int[61][61][61];
-	
-	public static void dfs(int depth, int hp1, int hp2, int hp3) {
-		int[] tmp = {hp1, hp2, hp3};
-		Arrays.sort(tmp);
-		
-		for (int i = 0; i < 3; i++) {
-			if (tmp[i] < 0) tmp[i] = 0;
-		}
-		
-		if (tmp[0] == 0 && tmp[1] == 0 && tmp[2] == 0) {
+
+	// scv의 현재 피를 인자로, 현재 몇 번째인지
+	public static void dfs(int a, int b, int c, int depth) {
+		if (a <= 0 && b <= 0 && c <= 0) {
 			answer = Math.min(answer, depth);
 			return;
 		}
 		
-		if (visited[tmp[0]][tmp[1]][tmp[2]] <= depth) return;
-		visited[tmp[0]][tmp[1]][tmp[2]] = depth;
-		
-		dfs(depth+1, tmp[0]-9, tmp[1]-3, tmp[2]-1);
-		dfs(depth+1, tmp[0]-9, tmp[1]-1, tmp[2]-3);
-		dfs(depth+1, tmp[0]-3, tmp[1]-9, tmp[2]-1);
-		dfs(depth+1, tmp[0]-3, tmp[1]-1, tmp[2]-9);
-		dfs(depth+1, tmp[0]-1, tmp[1]-9, tmp[2]-3);
-		dfs(depth+1, tmp[0]-1, tmp[1]-3, tmp[2]-9);
-		
-		
+		for (int i = 0; i < 6; i++) {
+			int nextA = Math.max(0, a-attack[i][0]);
+			int nextB = Math.max(0, b-attack[i][1]);
+			int nextC = Math.max(0, c-attack[i][2]);
+			
+			if (dp[nextA][nextB][nextC] <= depth+1) continue;
+			
+			dp[nextA][nextB][nextC] = depth+1;
+			dfs(nextA, nextB, nextC, depth+1);
+		}
 	}
-
+	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		scv = new int[3];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		scv = new int[3];
+		
 		for (int i = 0; i < N; i++) {
 			scv[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(scv);
+		
+		dp = new int[61][61][61];
 		
 		for (int i = 0; i < 61; i++) {
 			for (int j = 0; j < 61; j++) {
 				for (int k = 0; k < 61; k++) {
-					visited[i][j][k] = Integer.MAX_VALUE;
+					dp[i][j][k] = Integer.MAX_VALUE;
 				}
 			}
 		}
 		
-		dfs(0, scv[0], scv[1], scv[2]);
+		dp[scv[0]][scv[1]][scv[2]] = 0;
+		
+		dfs(scv[0], scv[1], scv[2], 0);
+		
 		System.out.println(answer);
 	}
 }
