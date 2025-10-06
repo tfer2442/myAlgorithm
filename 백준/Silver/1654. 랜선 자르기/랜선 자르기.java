07551c178d3457ff2d018b3f29bd1e43fc71a99d
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-10-06 17:34:21 +0900

    [Silver II] Title: 랜선 자르기, Time: 156 ms, Memory: 16744 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,50 +1,57 @@
 import java.util.*;
 import java.io.*;
 
+/*
+ * 1 <= 랜선의 개수 K <= 10,000
+ * 1 <= 필요한 랜선의 개수 N <= 1,000,000
+ * K <= N
+ * 1 <= 랜선의 길이 <= 2^31-1
+ */
 public class Main {
-    public static int K, N;
-    public static int[] nums;
-    
-    public static int calculate(long mid) {
-        int cnt = 0;
-        
-        for (int i = 0; i < K; i++) {
-            cnt += nums[i] / mid;
-        }
-        
-        return cnt;
-    }
+	public static int N, K;
+	public static long[] cable;
+	
+	public static long checkCableNum(long mid) {
+		long cnt = 0;
+		
+		for (int i = 0; i < K; i++) {
+			cnt += cable[i]/mid;
+		}
+		
+		return cnt;
+	}
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        K = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        
-        nums = new int[K];
-        
-        long left = 1;
-        long right = 0;  // 최대값으로 설정할 예정
-        
-        for (int i = 0; i < K; i++) {
-            int tmp = Integer.parseInt(br.readLine());
-            nums[i] = tmp;
-            right = Math.max(right, tmp);  // 입력된 랜선 중 최대값
-        }
-
-        long answer = 1;
-        while (left <= right) {
-            long mid = (left + right) / 2;  // 정수 나눗셈
-            int tmp = calculate(mid);
-                        
-            if (tmp >= N) {
-                answer = Math.max(answer, mid);
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        
-        System.out.println(answer);
-    }
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		K = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		
+		cable = new long[K];
+		
+		long left = 1;
+		long right = 0;
+		long answer = 1;
+		
+		for (int i = 0; i < K; i++) {
+			cable[i] = Integer.parseInt(br.readLine());
+			right = Math.max(right, cable[i]);
+		}
+		
+		while (left <= right) {
+			long mid = (left + right) / 2;
+			long tmp = checkCableNum(mid);
+			
+			if (tmp < N) {
+				right = mid - 1;
+			} else if (tmp >= N) {
+				answer = mid;
+				left = mid + 1;
+			}
+		}
+		
+		System.out.println(answer);
+	}
 }
