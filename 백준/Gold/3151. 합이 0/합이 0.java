9b65d999c2ddab0f9644efeb22bb8c622fd257b7
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-10-06 18:09:52 +0900

    [Gold IV] Title: 합이 0, Time: 2800 ms, Memory: 18856 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -3,72 +3,72 @@ import java.io.*;
 
 public class Main {
 	public static int N;
-	public static int[] scores;
-	
+	public static int[] students;
+
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
+		students = new int[N];
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		scores = new int[N];
 		
-		for (int i =0; i < N; i++) {
-			scores[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			students[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		if (N <= 2) {
-			System.out.println(0);
-			return;
-		}
+		Arrays.sort(students);
 		
-		Arrays.sort(scores);
 		long answer = 0;
 		
 		for (int i = 0; i < N-2; i++) {
 			for (int j = i+1; j < N-1; j++) {
-				int left = j + 1;
-				int right = N - 1;
-				int sum = scores[i] + scores[j];
-				int target = -1 * sum;
-				int leftIdx = N;
+				int left = j+1;
+				int right = N-1;
+				int value = students[i] + students[j];
+				int lowerBound = -1;
 				
-				// target 맨 왼쪽 인덱스 찾기
+				// lowerBound
 				while (left <= right) {
-					int mid = (left+right)/2;
+					int mid = (left + right) / 2;
+					int tmp = value + students[mid];
 					
-					if (scores[mid] > target) {
+					if (tmp > 0) {
 						right = mid - 1;
-					} else if (scores[mid] < target) {
+					} else if (tmp < 0) {
 						left = mid + 1;
-					} else if (scores[mid] == target) {
-						leftIdx = mid;
-						right = mid - 1;
+					} else if (tmp == 0) {
+						lowerBound = mid;
+						right = mid-1;
 					}
 				}
-								
-				// target 값이 없다면 반복문 넘어가기
-				if (leftIdx == N) continue;
+			
+				if (lowerBound == -1) {
+					continue;
+				}
 				
-				left = j + 1;
-				right = N - 1;
-				int rightIdx = -1;
-				// target 맨 오른쪽 인덱스 찾기
+				// upperBound
+				left = lowerBound;
+				right = N-1;
+				int upperBound = -1;
 				while (left <= right) {
-					int mid = (left+right)/2;
+					int mid = (left + right) / 2;
+					int tmp = value + students[mid];
 					
-					if (scores[mid] > target) {
+					if (tmp > 0) {
 						right = mid - 1;
-					} else if (scores[mid] < target) {
+					} else if (tmp < 0) {
 						left = mid + 1;
-					} else if (scores[mid] == target) {
-						rightIdx = mid;
-						left = mid + 1;
+					} else if (tmp == 0) {
+						upperBound = mid;
+						left = mid+1;
 					}
 				}
 				
-				answer += (rightIdx - leftIdx) + 1;
+				answer += (upperBound-lowerBound+1);
 			}
 		}
+		
 		System.out.println(answer);
 	}
 }
