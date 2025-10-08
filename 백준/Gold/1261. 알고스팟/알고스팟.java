3c4cfa5d42e99cc14cba48f848e05ac1b550ed72
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-10-08 16:56:49 +0900

    [Gold IV] Title: 알고스팟, Time: 120 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,56 +1,64 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
+
 
 public class Main {
 	public static int N, M;
 	public static int[][] board;
-	public static int[][] visited;
-	public static ArrayDeque<int[]> dq = new ArrayDeque<>();
-	public static int[][] d = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+	public static int[][] distance;
+	public static int[][] d = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
 	
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-		board = new int[N][M];
-		visited = new int[N][M];
+	public static void bfs() {
+		distance[0][0] = 0;
 		
-		for (int i = 0 ; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			String tmp = st.nextToken();
-			for (int j = 0; j < M; j++) {
-				board[i][j] = tmp.charAt(j) - '0';
-				visited[i][j] = Integer.MAX_VALUE;
-			}
-		}
+		ArrayDeque<int[]> dq = new ArrayDeque<>();
+		dq.addFirst(new int[] {0, 0});
 		
-		visited[0][0] = 0;
-//		visited[N-1][M-1] = 0;
-		
-		dq.add(new int[] {0, 0, 0});
 		while (!dq.isEmpty()) {
-			int[] node = dq.poll();
+			int[] node = dq.pollFirst();
+			
+			if (node[0] == N-1 && node[1] == M-1) return;
+			
 			
 			for (int[] dd : d) {
 				int nextR = node[0] + dd[0];
 				int nextC = node[1] + dd[1];
-
-				if (0 > nextR || nextR >= N || 0 > nextC || nextC >= M) continue;
 				
-				if (board[nextR][nextC] == 0 && node[2] < visited[nextR][nextC]) {
-					visited[nextR][nextC] = node[2];
-					dq.add(new int[] {nextR, nextC, node[2]});
-				} else if (board[nextR][nextC] == 1 && node[2]+1 < visited[nextR][nextC]) {
-					visited[nextR][nextC] = node[2] + 1;
-					dq.add(new int[] {nextR, nextC, node[2]+1});
+				if (nextR < 0 || nextR >= N || nextC < 0 || nextC >= M) continue;
+				if (distance[nextR][nextC] > distance[node[0]][node[1]] + board[nextR][nextC]) {
+					distance[nextR][nextC] = distance[node[0]][node[1]] + board[nextR][nextC];
+					
+					if (board[nextR][nextC] == 1) {
+						dq.addLast(new int[] {nextR, nextC});						
+					} else {
+						dq.addFirst(new int[] {nextR, nextC});						
+					}
 				}
 			}
 		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		System.out.println(visited[N-1][M-1]);
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		
+		board = new int[N][M];
+		distance = new int[N][M];
+		
+		for (int i = 0; i < N; i++) {
+			String tmp = br.readLine();
+			
+			for (int j = 0; j < M; j++) {
+				board[i][j] = tmp.charAt(j) - '0';
+			}
+			Arrays.fill(distance[i], Integer.MAX_VALUE);
+		}
+		
+		bfs();
+		System.out.println(distance[N-1][M-1]);
 	}
 }
