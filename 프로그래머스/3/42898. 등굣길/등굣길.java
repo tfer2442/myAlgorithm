6106030cea45b223a32fafadd2b075eaa4dd7510
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2025-12-21 14:36:52 +0900

    [level 3] Title: 등굣길, Time: 2.71 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,50 +1,56 @@
 import java.util.*;
 
 class Solution {
+    HashSet<Integer> hs = new HashSet<>();
     int[][] dp;
-    boolean[][] visited;
-    
+    static final int MOD = 1_000_000_007;
+
+    private int key(int r, int c, int m) {
+        return r * m + c;
+    }
+
     public int solution(int m, int n, int[][] puddles) {
-        visited = new boolean[n][m];
-    
+
         for (int i = 0; i < puddles.length; i++) {
-            visited[puddles[i][1]-1][puddles[i][0]-1] = true;
+            int r = puddles[i][1] - 1;
+            int c = puddles[i][0] - 1;
+            hs.add(key(r, c, m));
         }
-        
+
         dp = new int[n][m];
-        
+
         boolean flag = false;
-        
+
         for (int i = 0; i < n; i++) {
-            if (visited[i][0]) {
+            if (hs.contains(key(i, 0, m))) {
                 flag = true;
             }
-            
+
             if (flag) {
                 dp[i][0] = 0;
             } else {
                 dp[i][0] = 1;
             }
         }
-        
+
         flag = false;
-        
+
         for (int i = 0; i < m; i++) {
-            if (visited[0][i]) {
+            if (hs.contains(key(0, i, m))) {
                 flag = true;
             }
-            
+
             if (flag) {
                 dp[0][i] = 0;
             } else {
                 dp[0][i] = 1;
             }
         }
-        
+
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
-                if (visited[i][j]) continue;
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1_000_000_007;
+                if (hs.contains(key(i, j, m))) continue;
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
             }
         }
 
