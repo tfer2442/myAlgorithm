d5165584da10304bc147cb8449b32b85a3cba026
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-01-27 00:03:39 +0900

    [level 3] Title: 야근 지수, Time: 117.67 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -3,26 +3,26 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            return o2-o1;
+        });
         
         for (int i = 0; i < works.length; i++) {
             pq.add(works[i]);
         }
-        
+
         while (n > 0) {
-            int a = pq.poll();
+            int node = pq.poll();
             
-            if (a == 0) return 0;
+            if (node == 0) break;
             
-            pq.add(a-1);
+            pq.add(node-1);
             n--;
         }
         
         while (!pq.isEmpty()) {
-            int value = pq.poll();
-            
-            answer += value * value; 
+            int node = pq.poll();
+            answer += (long)node * node;
         }
         
         return answer;
