5b3d776eb777f06c4cd3f171b4ca192a6c97f43c
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-02-06 11:26:38 +0900

    [level 2] Title: 택배상자, Time: 48.68 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,40 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        int answer = 0;
-        int j = 1;
         ArrayDeque<Integer> dq = new ArrayDeque<>();
         
-        for (int i = 0; i < order.length; i++) {
-            // order[i]에 있는 값을 찾아야 함. 
-            if (order[i] == j) {
-                answer++;
-                j++;
-                continue;
-            }
-            
-            // 만약 order[i] != j 라면, stack or 순서에서 값을 찾아야 함.
-            // order[i]와 j의 값을 비교하여, order[i] > j 라면 j++ 해야 함. 그 전에, j를 스택에 넣어야 함.
-            while (order[i] > j) {
-                dq.addLast(j);
-                j++;
-            }
-            
-            if (order[i] == j) {
-                answer++;
-                j++;
-                continue;
-            }
-            
-            if (dq.isEmpty()) break;
-            
-            if (order[i] == dq.pollLast()) {
-                answer++;
-            } else {
+        // order의 idx를 보면서, 1~N까지 움직이는 반복문도 있어야 함
+        
+        int cnt = 0;
+        int orderIdx = 0;
+        int i = 1;
+        
+        while (orderIdx < order.length) {
+            if (order[orderIdx] == i) {
+                cnt++;
+                orderIdx++;
+                i++;
+            } else if (!dq.isEmpty() && dq.getLast() == order[orderIdx]) {
+                dq.pollLast();
+                cnt++;
+                orderIdx++;
+            } else if (order[orderIdx] > i) {
+                dq.addLast(i);
+                i++;
+            } else if (order[orderIdx] < i) {
                 break;
             }
         }
         
-        return answer;
+        return cnt;
     }
 }
