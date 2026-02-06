068f05eea7c9520920898e6267165c6f41f631c3
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-02-06 11:36:27 +0900

    [level 2] Title: 택배상자, Time: 52.40 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,27 +3,22 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         ArrayDeque<Integer> dq = new ArrayDeque<>();
-        
-        // order의 idx를 보면서, 1~N까지 움직이는 반복문도 있어야 함
-        
         int cnt = 0;
-        int orderIdx = 0;
-        int i = 1;
-        
-        while (orderIdx < order.length) {
-            if (order[orderIdx] == i) {
-                cnt++;
-                orderIdx++;
-                i++;
-            } else if (!dq.isEmpty() && dq.getLast() == order[orderIdx]) {
-                dq.pollLast();
-                cnt++;
-                orderIdx++;
-            } else if (order[orderIdx] > i) {
-                dq.addLast(i);
-                i++;
-            } else if (order[orderIdx] < i) {
-                break;
+        int idx = 0;
+    
+        for (int i = 0; i < order.length; i++) {
+            dq.addLast(i+1);
+            
+            while (!dq.isEmpty()) {
+                if (order[idx] == dq.getLast()) {
+                    idx++;
+                    dq.pollLast();
+                    cnt++;
+                } else if (order[idx] < dq.getLast()){
+                    return cnt;
+                } else {
+                    break;
+                }
             }
         }
         
