29dec59ea5ab7bacc9f03d35198ace6fd00fbc41
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-02-07 14:30:45 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.18 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,21 +2,35 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
+        String[] str = s.split(" ", -1);
         StringBuilder sb = new StringBuilder();
-        
-        String[] sArr = s.split(" ", -1); // 빈 문자열도 유지
 
-        for (int i = 0; i < sArr.length; i++) {
-            String tmp = sArr[i]; // strip() 제거 (공백 보존 목적)
-
-            if (tmp.length() > 0) {
-                sb.append(Character.toUpperCase(tmp.charAt(0)));
-                sb.append(tmp.substring(1).toLowerCase()); // 길이 1이면 "" 붙는 거라 OK
+        for (int i = 0; i < str.length; i++) {
+            String curS = str[i].toLowerCase();
+            
+            if (curS.equals("")) {
+                if (i != str.length-1) {
+                    sb.append(" ");
+                }
+                continue;
             }
-
-            if (i != sArr.length - 1) sb.append(" "); // 토큰 사이 공백 1개 = 원본 공백 개수 보존됨
+            
+            char c = curS.charAt(0);
+            
+            if (c >= 'a' && c <= 'z') {
+                c = (char)(c + ('A' - 'a'));
+            }
+            
+            sb.append(c);
+            
+            if (curS.length() >= 2) {
+                sb.append(curS.substring(1));
+            }
+            
+            if (i != str.length-1) {
+                sb.append(" ");
+            }
         }
-
         
         return sb.toString();
     }
