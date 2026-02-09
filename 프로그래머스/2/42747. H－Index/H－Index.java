83a5fe74a7b769cea75b227133bcd32109204b47
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-02-09 20:01:57 +0900

    [level 2] Title: H-Index, Time: 1.08 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,6 +1,32 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        Arrays.sort(citations);
+        
+        int j = 0;
+        boolean flag = false;
+        
+        for (int i = 0; i <= 1000; i++) {
+            
+            while (j < citations.length) {
+                if (i > citations.length - j) {
+                    flag = true;
+                    break;
+                }
+                
+                if (i <= citations[j]) {
+                    answer = Math.max(answer, i);
+                    break;
+                } else {
+                    j++;
+                }
+            }
+            
+            if (flag) break;
+        }
+        
         return answer;
     }
 }
