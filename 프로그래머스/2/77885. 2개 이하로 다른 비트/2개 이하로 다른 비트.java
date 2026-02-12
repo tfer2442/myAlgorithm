f0cfd1d3542592c0aaba443639b946817219d7d9
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-02-12 20:27:29 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 15.15 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -5,13 +5,22 @@ class Solution {
         long[] answer = new long[numbers.length];
         
         for (int i = 0; i < numbers.length; i++) {
-            long x = numbers[i] + 1;
-            long diff = numbers[i] ^ x;
-            long value = x + (diff >> 2);
-            
-            answer[i] = value;
+            if (numbers[i] % 2 == 0) {
+                answer[i] = numbers[i] + 1;
+            } else {
+                int j = 1;
+                
+                while (true) {
+                    if ((numbers[i] & (1L << j)) == 0) {
+                        
+                        answer[i] = (numbers[i] - (1L << (j-1))) + (1L << j);
+                        break;
+                    }
+                    
+                    j++;
+                }
+            }
         }
-        
         
         return answer;
     }
