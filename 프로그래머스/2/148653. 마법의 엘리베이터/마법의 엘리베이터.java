7bc9f51d1ca6248a0067aa9c84edb85da0c616ba
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-02-14 16:14:03 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,27 +1,29 @@
+import java.util.*;
+
 class Solution {
-    int answer = Integer.MAX_VALUE;
-    
-    void dfs(int storey, int cnt) {
-        if (cnt >= answer) {
-            return;
+    public int solution(int storey) {
+        int answer = 0;
+        
+        while (storey > 0) {
+            int a = storey % 10;
+            int b = storey / 10;
+            
+            if (a > 5) {
+                storey = b + 1;
+                answer += 10 - a;
+            } else if (a < 5) {
+                storey = b;
+                answer += a;
+            } else {
+                if (b % 10 >= 5) {
+                    storey = b + 1;
+                    answer += 10-a;
+                } else {
+                    storey = b;
+                    answer += a;
+                }
+            }
         }
-        if (storey == 0) {
-            answer = cnt;
-            return;
-        }
-        
-        int last = storey % 10;
-        
-        
-        // 맨 마지막 자리를 버림
-        dfs(storey/10 , cnt + last);
-        
-        // 맨 마지막 자리를 올림
-        dfs(storey/10 + 1, cnt + (10-last));
-    }
-    
-    public int solution(int storey) { 
-        dfs(storey, 0);
         
         return answer;
     }
