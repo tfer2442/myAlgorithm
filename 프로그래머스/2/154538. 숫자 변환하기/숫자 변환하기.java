a208cdaa59bef951c9bd0d07312a1e4b8d8d1325
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-02-15 15:28:33 +0900

    [level 2] Title: 숫자 변환하기, Time: 65.30 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,31 +1,40 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int x, int y, int n) {
-        boolean[] visited = new boolean[y+1];
+
+        boolean[] visited = new boolean[1_000_001];
         
-        // [0] : depth, [1] : 현재 x
         ArrayDeque<int[]> dq = new ArrayDeque<>();
-        
-        dq.add(new int[]{0, x});
+        dq.add(new int[]{x, 0});
+        visited[x] = true;
         
         while (!dq.isEmpty()) {
             int[] node = dq.poll();
+            int value = node[0];
+            int depth = node[1];
             
-            if (node[1] > y) continue;
-            if (visited[node[1]]) continue;
-            visited[node[1]] = true;
-            
-            if (node[1] == y) {
-                return node[0];
+            if (value == y) {
+                return depth;
             }
             
-            dq.add(new int[]{node[0]+1, node[1]+n});
-            dq.add(new int[]{node[0]+1, node[1]*2});
-            dq.add(new int[]{node[0]+1, node[1]*3});
+            if (value + n <= y && !visited[value+n]) {
+                visited[value+n] = true;
+                dq.add(new int[]{value+n, depth+1});
+            }
+            
+            if (value * 2 <= y && !visited[value*2]) {
+                visited[value*2] = true;
+                dq.add(new int[]{value*2, depth+1});
+            }
+            
+            if (value * 3 <= y && !visited[value*3]) {
+                visited[value*3] = true;
+                dq.add(new int[]{value*3, depth+1});
+            }
+            
         }
-                
+        
         return -1;
     }
 }
