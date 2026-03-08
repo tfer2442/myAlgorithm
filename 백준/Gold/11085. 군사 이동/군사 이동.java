861daaff3371646020eb51d24eda8c9c66623776
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-03-08 17:12:10 +0900

    [Gold III] Title: 군사 이동, Time: 296 ms, Memory: 32884 KB -BaekjoonHub

--- 백준/Gold/11085. 군사 이동/군사 이동.java
+++ 백준/Gold/11085. 군사 이동/군사 이동.java
@@ -2,6 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	static int[] parent;
+	
+	static int find(int x) {
+		if (parent[x] == x) {
+			return x;
+		}
+		
+		return parent[x] = find(parent[x]);
+	}
+	
+	static boolean union(int a, int b) {
+		int parentA = find(a);
+		int parentB = find(b);
+		
+		if (parentA == parentB) return false;
+		parent[parentA] = parentB;
+		
+		return true;
+	}
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
@@ -14,71 +33,37 @@ public class Main {
 		int c = Integer.parseInt(st.nextToken());
 		int v = Integer.parseInt(st.nextToken());
 		
-		ArrayList<int[]>[] graph = new ArrayList[p];
-		int[][] distance = new int[p][p];
-		
-		for (int i = 0; i < p; i++) {
-			graph[i] = new ArrayList<>();
-		}
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+			return o2[0]-o1[0];
+		});
 		
 		for (int i = 0; i < w; i++) {
 			st = new StringTokenizer(br.readLine());
+
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int value = Integer.parseInt(st.nextToken());
 			
-			graph[a].add(new int[] {b, value});
-			graph[b].add(new int[] {a, value});
-			distance[a][b] = Math.max(distance[a][b], value);
-			distance[b][a] = Math.max(distance[b][a], value);
+			pq.add(new int[] {value, a, b});
 		}
 		
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2)->{
-			return o2[1] - o1[1];
-		});
+		parent = new int[p];
 		
-		pq.add(new int[] {c, 0, -1});
-		boolean[] visited = new boolean[p];
+		for (int i = 0; i < p; i++) {
+			parent[i] = i;
+		}
 		
-		ArrayList<Integer>[] path = new ArrayList[p];
-		
-		path[c] = new ArrayList<>();
-		
-		while (!pq.isEmpty()) {
+		for (int i = 0; i < w; i++) {
 			int[] node = pq.poll();
-			int vertex = node[0];
-			int value = node[1];
-			int prev = node[2];
+			int a = node[1];
+			int b = node[2];
 			
-			if (visited[vertex]) continue;
-			visited[vertex] = true;
+			union(a, b);
 			
-			if (vertex != c) {
-				path[vertex] = new ArrayList<>();
-				path[vertex].add(prev);
-				
-				for (int a : path[prev]) {
-					path[vertex].add(a);
-				}
-			}
-			
-			for (int[] nextNode : graph[vertex]) {
-				int nextVertex = nextNode[0];
-				int nextValue = nextNode[1];
-				
-				if (visited[nextVertex]) continue;
-				pq.add(new int[] {nextVertex, nextValue, vertex});
+			if (find(c) == find(v)) {
+				System.out.println(node[0]);
+				break;
 			}
 		}
-				
-		int answer = Integer.MAX_VALUE;
-		int cur = v;
-		
-		for (int i = 0; i < path[v].size(); i++) {
-			answer = Math.min(answer, distance[cur][path[v].get(i)]);
-			cur = path[v].get(i); 
-		}
-		
-		System.out.println(answer);
 	}
 }
