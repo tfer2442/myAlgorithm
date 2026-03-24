349830e3b7d09ddd3e5ea85f34da4ecf345a4129
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-03-25 00:04:33 +0900

    [level 2] Title: 스킬트리, Time: 0.04 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -3,34 +3,31 @@ import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        int[] skill_idx = new int[26];
+        int[] nums = new int[26];
         
-        for (int i = 1; i <= skill.length(); i++) {
-            skill_idx[skill.charAt(i-1) - 'A'] = i;
+        for (int i = 0; i < skill.length(); i++) {
+            nums[skill.charAt(i) - 'A'] = i+1;
         }
         
         for (int i = 0; i < skill_trees.length; i++) {
-            String s = skill_trees[i];
-            int sum = 1;
+            String cur = skill_trees[i];
+            int cnt = 1;
             boolean flag = false;
             
-            for (int j = 0; j < s.length(); j++) {
-                if (skill_idx[s.charAt(j)-'A'] == 0) continue;
-                else if (skill_idx[s.charAt(j)-'A'] == sum) {
-                    sum++;
-                    continue;
-                }
-                else {
+            for (int j = 0; j < cur.length(); j++) {
+                if (nums[cur.charAt(j)-'A'] == 0) continue;
+                
+                if (nums[cur.charAt(j)-'A'] != cnt) {
                     flag = true;
                     break;
                 }
+                cnt++;
             }
             
             if (!flag) {
                 answer++;
             }
         }
-        
         
         return answer;
     }
