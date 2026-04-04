094ca9313d521bd7c833bd0f8231676b87c96be7
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-04-04 15:50:32 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,52 +1,138 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static int[][] board;
-	public static boolean[][] visited;
 	public static int N;
-	public static int[][] d = {{0, 1}, {1, 1}, {1, 0}};
-	public static int answer = 0;
+	public static int[][] board;
+	public static int[][][] dp;
 	
-	public static void dfs(int cRow, int cCol,int cD) {
-		if (cRow == N-1 && cCol == N-1) {
-			answer++;
-			return;
+	public static boolean isPossiblePipe(int r, int c, int curD) {
+		if (curD == 0) {
+			if (c+1 >= N) return false;
+			if (board[r][c+1] == 1) return false;
+		} else if (curD == 1) {
+			if (r+1 >= N) return false;
+			if (board[r+1][c] == 1) return false;
+			
+		} else if (curD == 2) {
+			if (r+1 >= N || c+1 >= N) return false;
+			if (board[r+1][c] == 1 || board[r][c+1] == 1 || board[r+1][c+1] == 1) return false;
 		}
 		
-		for (int i = 0; i < 3; i++) {
-			if (cD == 0 && i == 2) continue;
-			if (cD == 2 && i == 0) continue;
-			int nRow = cRow + d[i][0];
-			int nCol = cCol + d[i][1];
-			
-			if (0 > nRow || nRow >= N || 0 > nCol || nCol >= N) continue;
-			if (visited[nRow][nCol]) continue;
-			if (board[nRow][nCol]==1) continue;
-			if (i==1 && (board[nRow-1][nCol]==1 || board[nRow][nCol-1]==1)) continue;
-			visited[nRow][nCol] = true;
-			dfs(nRow, nCol, i);
-			visited[nRow][nCol] = false;
+		return true;
+	}
+	
+	public static void putPipe(int r, int c, int curD) {
+		if (curD == 0) {
+			board[r][c+1] = 1;
+		} else if (curD == 1) {
+			board[r+1][c] = 1;
+		} else if (curD == 2) {
+			board[r+1][c+1] = 1;
 		}
 	}
+	
+	public static void undoPipe(int r, int c, int curD) {
+		if (curD == 0) {
+			board[r][c+1] = 0;
+		} else if (curD == 1) {
+			board[r+1][c] = 0;
+		} else if (curD == 2) {
+			board[r+1][c+1] = 0;
+		}
+	}
+	
+	// 0 : 가로, 1 : 세로, 2 : 대각선
+	
+	public static int calculate(int r, int c, int prevD) {
+		if (r == N-1 && c == N-1) {
+			return 1;
+		}
+		
+		if (dp[r][c][prevD] != -1) {
+			return dp[r][c][prevD];
+		}
+		
+		// prevD를 기준으로 놓을 수 있는 파이프를 찾자
+		if (prevD == 0) {
+			// D 0, 2 가능
+			if (isPossiblePipe(r, c, 0)) {
+				putPipe(r, c, 0);
+				dp[r][c][prevD] += calculate(r, c+1, 0);
+				undoPipe(r, c, 0);
+			}
+			
+			if (isPossiblePipe(r, c, 2)) {
+				putPipe(r, c, 2);
+				dp[r][c][prevD] += calculate(r+1, c+1, 2);
+				undoPipe(r, c, 2);
+			}
+			
+		} else if (prevD == 1) {
+			// D 1, 2 가능			
+			if (isPossiblePipe(r, c, 1)) {
+				putPipe(r, c, 1);
+				dp[r][c][prevD] += calculate(r+1, c, 1);
+				undoPipe(r, c, 1);
+			}
+			
+			if (isPossiblePipe(r, c, 2)) {
+				putPipe(r, c, 2);
+				dp[r][c][prevD] += calculate(r+1, c+1, 2);
+				undoPipe(r, c, 2);
+			}
+			
+		} else if (prevD == 2) {
+			// D 0, 1, 2 다 가능
+			
+			if (isPossiblePipe(r, c, 0)) {
+				putPipe(r, c, 0);
+				dp[r][c][prevD] += calculate(r, c+1, 0);
+				undoPipe(r, c, 0);
+			}
+			
+			if (isPossiblePipe(r, c, 1)) {
+				putPipe(r, c, 1);
+				dp[r][c][prevD] += calculate(r+1, c, 1);
+				undoPipe(r, c, 1);
+			}
+			
+			if (isPossiblePipe(r, c, 2)) {
+				putPipe(r, c, 2);
+				dp[r][c][prevD] += calculate(r+1, c+1, 2);		
+				undoPipe(r, c, 2);
+			}
+		}
+		
+		
+		return dp[r][c][prevD] += 1;
+	}
+
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
-		visited = new boolean[N][N];
 		
+		board = new int[N][N];
+		dp = new int[N][N][3];
+		
+		StringTokenizer st;
 		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
+				
+				for (int k = 0; k < 3; k++) {
+					dp[i][j][k] = -1;
+				}
 			}
 		}
-		visited[0][0] = true;
-		visited[0][1] = true;
-		dfs(0, 1, 0);
-		System.out.println(answer);
+		
+		board[0][0] = 1;
+		board[0][1] = 1;
+		calculate(0, 1, 0);
+		
+		System.out.println(dp[0][1][0]);
 	}
 
 }
