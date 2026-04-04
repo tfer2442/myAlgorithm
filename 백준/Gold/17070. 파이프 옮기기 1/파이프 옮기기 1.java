ec41d54bfea7f1b04e5e4d091dbfe75f0f5a1400
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-04-04 15:56:47 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 108 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -22,26 +22,6 @@ public class Main {
 		return true;
 	}
 	
-	public static void putPipe(int r, int c, int curD) {
-		if (curD == 0) {
-			board[r][c+1] = 1;
-		} else if (curD == 1) {
-			board[r+1][c] = 1;
-		} else if (curD == 2) {
-			board[r+1][c+1] = 1;
-		}
-	}
-	
-	public static void undoPipe(int r, int c, int curD) {
-		if (curD == 0) {
-			board[r][c+1] = 0;
-		} else if (curD == 1) {
-			board[r+1][c] = 0;
-		} else if (curD == 2) {
-			board[r+1][c+1] = 0;
-		}
-	}
-	
 	// 0 : 가로, 1 : 세로, 2 : 대각선
 	
 	public static int calculate(int r, int c, int prevD) {
@@ -53,59 +33,47 @@ public class Main {
 			return dp[r][c][prevD];
 		}
 		
+		dp[r][c][prevD] = 0;
+		
 		// prevD를 기준으로 놓을 수 있는 파이프를 찾자
 		if (prevD == 0) {
 			// D 0, 2 가능
 			if (isPossiblePipe(r, c, 0)) {
-				putPipe(r, c, 0);
 				dp[r][c][prevD] += calculate(r, c+1, 0);
-				undoPipe(r, c, 0);
 			}
 			
 			if (isPossiblePipe(r, c, 2)) {
-				putPipe(r, c, 2);
 				dp[r][c][prevD] += calculate(r+1, c+1, 2);
-				undoPipe(r, c, 2);
 			}
 			
 		} else if (prevD == 1) {
 			// D 1, 2 가능			
 			if (isPossiblePipe(r, c, 1)) {
-				putPipe(r, c, 1);
 				dp[r][c][prevD] += calculate(r+1, c, 1);
-				undoPipe(r, c, 1);
 			}
 			
 			if (isPossiblePipe(r, c, 2)) {
-				putPipe(r, c, 2);
 				dp[r][c][prevD] += calculate(r+1, c+1, 2);
-				undoPipe(r, c, 2);
 			}
 			
 		} else if (prevD == 2) {
 			// D 0, 1, 2 다 가능
 			
 			if (isPossiblePipe(r, c, 0)) {
-				putPipe(r, c, 0);
 				dp[r][c][prevD] += calculate(r, c+1, 0);
-				undoPipe(r, c, 0);
 			}
 			
 			if (isPossiblePipe(r, c, 1)) {
-				putPipe(r, c, 1);
 				dp[r][c][prevD] += calculate(r+1, c, 1);
-				undoPipe(r, c, 1);
 			}
 			
 			if (isPossiblePipe(r, c, 2)) {
-				putPipe(r, c, 2);
 				dp[r][c][prevD] += calculate(r+1, c+1, 2);		
-				undoPipe(r, c, 2);
 			}
 		}
 		
 		
-		return dp[r][c][prevD] += 1;
+		return dp[r][c][prevD];
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -127,12 +95,9 @@ public class Main {
 				}
 			}
 		}
-		
-		board[0][0] = 1;
-		board[0][1] = 1;
+
 		calculate(0, 1, 0);
 		
 		System.out.println(dp[0][1][0]);
 	}
-
 }
