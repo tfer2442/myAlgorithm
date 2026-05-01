b9bc8cc8d24142bd2e88382aa4844314387eaef5
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-05-01 17:14:38 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 142 ms, Memory: 38,860 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -1,63 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Iterator;
+import java.util.*;
+import java.io.*;
 
-class Solution {
-	public static int[][] arr;
-	public static int sRow;
-	public static int sCol;
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
-		for (int T = 1; T <= 10; T++) {
-			int n = Integer.parseInt(br.readLine());
-			arr = new int[100][100];
+public class Solution {
+	public static int T, N;
+	public static int[][] d = {{0, -1}, {0, 1}, {1, 0}};
+	public static int[][] board;
+	public static boolean[][] visited;
+	
+	public static void solution(int t) {
+		for (int x = 0; x < N; x++) {
+			if (board[0][x] == 0) continue;
 			
-			for (int i = 0; i < 100; i++) {
-				String[] tmp = br.readLine().split(" ");
+			// r, c
+			ArrayDeque<int[]> dq = new ArrayDeque<>();
+			
+			visited = new boolean[N][N];
+			
+			visited[0][x] = true;
+			visited[1][x] = true;
+			dq.add(new int[] {1, x});
+
+			
+			while (!dq.isEmpty()) {
+				int[] node = dq.poll();
+				int r = node[0];
+				int c = node[1];
 				
-				for (int j = 0; j < 100; j++) {
-					arr[i][j] = Integer.parseInt(tmp[j]);
-					if (arr[i][j] == 2) {
-						sRow = i;
-						sCol = j;
-					}
+				if (board[r][c] == 2) {
+					System.out.println("#" + t + " " + x);
+					return;
 				}
-			}
-			
-			int r = sRow-1;
-			int c = sCol;
-
-			while (true) {
-				if (r == 0) {
-					System.out.println("#" + T + " " + c);
+				
+				if (r == N-1) break;
+				
+				for (int i = 0; i < 3; i++) {
+					int nr = d[i][0] + r;
+					int nc = d[i][1] + c;
+					
+					if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+					if (board[nr][nc] == 0) continue;
+					if (visited[nr][nc]) continue;
+					
+					dq.add(new int[] {nr, nc});
+					visited[nr][nc] = true;
 					break;
 				}
-				if (c-1 >= 0 && arr[r][c-1] == 1) {
-					for(;c>=1; c--) {
-						if (arr[r][c-1] == 0) {
-							break;
-						}
-					}
-					r = r-1;
-					continue;
-				}
-				
-				if (c+1 < 100 && arr[r][c+1] == 1) {
-					for(;c+1<100; c++) {
-						if (arr[r][c+1] == 0) {
-							break;
-						}
-					}
-					r = r-1;
-					continue;
-				}
-				r = r-1;
 			}
+			
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = 100;
+		
+		for (int t = 1; t <= 10; t++) {
+			T = Integer.parseInt(br.readLine());
+			board = new int[N][N];
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				
+				for (int j = 0; j < N; j++) {
+					board[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			solution(t);
 		}
 	}
 
