f2683a946c338970702e4b8f39059d75d89753bf
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-05-19 16:43:22 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 137 ms, Memory: 36,828 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -22,64 +22,25 @@ public class Solution {
 				}
 			}
 			
-			// 1이 N극 2가 S극
-			// j는 열
-			// N 자성체부터 아래로 쭉 내리기
-			for (int j = 0; j < N; j++) {
-				for (int i = N-1; i >= 0; i--) {
-					if (board[i][j] != 1) continue;
-					
-					// 만약 1이라면? 그 위치부터 쭉 떨어트려야 함
-					
-					boolean flag = false;
-					for (int k = i+1; k < N; k++) {
-						if (board[k][j] != 0) {
-						    if (k - 1 != i) {
-						        board[k-1][j] = 1;
-						        board[i][j] = 0;
-						    }
-							flag = true;
-							break;
-						}
-					}
-					
-					if (!flag) {
-						board[i][j] = 0;
-					}
-				}
-			}
-			
-			// S자성체 위로 올리기
-			for (int j = 0; j < N; j++) {
-				for (int i = 0; i < N; i++) {
-					if (board[i][j] != 2) continue;
-					
-					// 만약 2이라면? 그 위치부터 쭉 올려야 함
-					
-					boolean flag = false;
-					for (int k = i-1; k >= 0; k--) {
-						if (board[k][j] != 0) {
-						    if (k + 1 != i) {
-						        board[k+1][j] = 2;
-						        board[i][j] = 0;
-						    }
-							flag = true;
-							break;
-						}
-					}
-					
-					if (!flag) {
-						board[i][j] = 0;
-					}
-				}
-
-			}
-			
 			int answer = 0;
 			for (int j = 0; j < N; j++) {
-				for (int i = 0; i < N-1; i++) {
-					if (board[i][j] == 1 && board[i+1][j] == 2) {
+				int value = 0;
+
+				for (int i = 0; i < N; i++) {
+					value += board[i][j];
+					
+					if (value == 2) {
+						if (board[i][j] == 2)  {
+							value = 0;
+						} else if (board[i][j] == 1) {
+							value = 1;
+						}
+						continue;
+					}
+					
+					if (value == 3) {
 						answer++;
+						value = 0;
 					}
 				}
 			}
