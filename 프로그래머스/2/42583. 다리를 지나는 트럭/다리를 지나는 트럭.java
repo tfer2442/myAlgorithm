95eabffe61f3c0ee02267f1a0037d3e9543b0d86
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-07-13 21:06:28 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 5.30 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,38 +1,49 @@
 import java.util.*;
 
 class Solution {
+    /* dq를 이용해서 풀 수 있을 거 같음
+    다리에 올라가 있는 트럭의 무게를 누적합으로 가지고 있기
+    다리를 건너고 있는 트럭을 담는 dq를 만들자 ((트럭 무게, 다리에 올라온 시간)을 담자)
+    */
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        // q 원소: [0] = 트럭 무게, [1] = 내려오는 시간(exitTime)
-
-        int time = 1;      // 첫 트럭은 time=1 시점(1초 지난 뒤)에 올라갈 수 있게 맞춤
-        int idx = 0;
-        int total = 0;
-
-        while (idx < truck_weights.length || !q.isEmpty()) {
-
-            // 현재 time에 내려오는 트럭들 먼저 처리
-            if (!q.isEmpty() && q.peek()[1] == time) {
-                total -= q.poll()[0];
+        int sum = 0;
+        
+        ArrayDeque<int[]> dq = new ArrayDeque<>();
+        
+        int i = 0;
+        int time = 1;
+        
+        // 대기 트럭을 다리에 모두 올릴 때까지 시행
+        while (i < truck_weights.length) {
+            // 다리를 건넌 트럭이 있다면 모두 내리기
+            
+            while (!dq.isEmpty()) {
+                if (dq.getFirst()[1]+bridge_length <= time) {
+                    int[] node = dq.pollFirst();
+                    sum -= node[0];
+                } else {
+                    break;
+                }
             }
-
-            // 아직 올릴 트럭이 남아 있고, 지금 올릴 수 있으면 올린다
-            if (idx < truck_weights.length && total + truck_weights[idx] <= weight) {
-                int w = truck_weights[idx];
-                total += w;
-                q.add(new int[]{w, time + bridge_length});
-                idx++;
-
-                // 규칙상 1초에 1대만 진입 가능 → 다음 시도를 위해 1초 진행
-                time++;
-            } else {
-                // 못 올리는 경우:
-                // (1) 다리에 트럭이 있으면 다음 "하차 이벤트" 시점으로 점프
-                if (!q.isEmpty()) time = q.peek()[1];
+            
+            // 트럭 올리기
+            if (weight >= sum + truck_weights[i]) {
+                sum += truck_weights[i];
+                dq.add(new int[]{truck_weights[i], time});
+                i++;
             }
+            
+            // 시간++
+            time++;
         }
-
+        
+        while (!dq.isEmpty()) {
+            int[] node = dq.pollFirst();
+            time = node[1] + bridge_length;
+        }
+        
+        // 다리를 건너고 있는 트럭 처리
+        
         return time;
     }
 }
-
