f2de2d0ed995b0b33524f5ab8667eb43d6237222
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-07-13 21:32:24 +0900

    [level 2] Title: 배달, Time: 3.37 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,68 +1,58 @@
 import java.util.*;
 
+/*
+시작점이 정해져있는 최단거리 문제 -> 다익스트라로 해결 가능
+*/
 class Solution {
-    ArrayList<int[]>[] graph;
-    int[] distance;
     
-    void dijkstra() {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2)-> {
-            return Integer.compare(o1[1], o2[1]);
-        });
+    public int solution(int N, int[][] road, int K) {
+        int[] distance = new int[N+1];
         
         Arrays.fill(distance, 1_000_000_000);
         distance[1] = 0;
         
-        pq.add(new int[]{1, 0});
-        
-        while (!pq.isEmpty()) {
-            int[] node = pq.poll();
-            int v = node[0];
-            int value = node[1];
-            
-            if (value > distance[v]) {
-                continue;
-            }
-            
-            for (int i = 0; i < graph[v].size(); i++) {
-                int[] nextNode = graph[v].get(i);
-                int nextV = nextNode[0];
-                int nextValue = value + nextNode[1];
-                
-                if (nextValue < distance[nextV]) {
-                    distance[nextV] = nextValue;
-                    pq.add(new int[]{nextV, nextValue});
-                }
-            }
-        }
-    }
-    
-    public int solution(int N, int[][] road, int K) {
-        
-        graph = new ArrayList[N+1];
+        ArrayList<int[]>[] graph = new ArrayList[N+1];
         
         for (int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
-
+        
         for (int i = 0; i < road.length; i++) {
-            int a = road[i][0];
-            int b = road[i][1];
-            int value = road[i][2];
+            int[] tmp = road[i];
+            int a = tmp[0];
+            int b = tmp[1];
+            int value = tmp[2];
             
             graph[a].add(new int[]{b, value});
             graph[b].add(new int[]{a, value});
         }
         
-        distance = new int[N+1];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2)->{
+            return o1[1]-o2[1]; 
+        });
         
-        dijkstra();
+        
+        pq.add(new int[]{1, 0});
+        
+        while (!pq.isEmpty()) {
+            int[] node = pq.poll();
+            
+            if (node[1] < distance[node[0]]) continue;
+            
+            for (int[] next : graph[node[0]]) {
+                int v = next[0];
+                int value = next[1] + distance[node[0]];
+                
+                if (distance[v] > value) {
+                    distance[v] = value;
+                    pq.add(new int[]{v, value});
+                }
+            }
+        }
         
         int answer = 0;
-
         for (int i = 1; i <= N; i++) {
-            if (distance[i] <= K) {
-                answer++;
-            }
+            if (distance[i] <= K) answer++;
         }
 
         return answer;
