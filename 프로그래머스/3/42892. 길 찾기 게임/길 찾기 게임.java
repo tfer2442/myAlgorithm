30084dfabfcd61eb176f468eab0f1db911c24971
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-07-15 01:04:14 +0900

    [level 3] Title: 길 찾기 게임, Time: 12.68 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,89 +1,94 @@
 import java.util.*;
 
 class Solution {
-    ArrayList<Integer> preorderResult, postorderResult;
+    int[][] nodes;
+    int N;
+    ArrayList<Integer> preorderList, postorderList;
     
     static class Node {
-        int x, y, idx;
         Node left, right;
+        int v, x, y;
         
-        Node(int x, int y, int idx) {
+        Node(int x, int y, int v) {
+            this.v = v;
             this.x = x;
             this.y = y;
-            this.idx = idx;
+        }
+    }
+    
+    void insert(Node cur, Node a) {
+        while (true) {
+            if (cur.x < a.x) {
+                if (cur.right == null) {
+                    cur.right = a;
+                    return;
+                }
+                
+                cur = cur.right;
+            }
+            
+            if (cur.x > a.x) {
+                if (cur.left == null) {
+                    cur.left = a;
+                    return;
+                }
+                
+                cur = cur.left;
+            }
         }
     }
     
     void preorder(Node node) {
         if (node == null) return;
-        preorderResult.add(node.idx);
+        
+        preorderList.add(node.v);
         preorder(node.left);
         preorder(node.right);
     }
     
     void postorder(Node node) {
         if (node == null) return;
+        
         postorder(node.left);
         postorder(node.right);
-        postorderResult.add(node.idx);
-    }
-    
-    void insert(Node root, Node child) {
-        Node cur = root;
-        
-        while (true) {
-            if (cur.x > child.x) {
-                if (cur.left == null) {
-                    cur.left = child;
-                    return;
-                }
-                cur = cur.left;
-            } else {
-                if (cur.right == null) {
-                    cur.right = child;
-                    return;
-                }
-                cur = cur.right;
-            }
-        }
+        postorderList.add(node.v);
     }
     
     public int[][] solution(int[][] nodeinfo) {
+        N = nodeinfo.length;
+        nodes = new int[N][3];
         
-        ArrayList<Node> tree = new ArrayList<>();
-        
-        for (int i = 0; i < nodeinfo.length; i++) {
-            Node node = new Node(nodeinfo[i][0], nodeinfo[i][1], i+1);
-            tree.add(node);
+        for (int i = 0; i < N; i++) {
+            nodes[i] = new int[]{nodeinfo[i][0], nodeinfo[i][1], i+1};
         }
         
-        tree.sort((o1, o2) -> {
-            if (o2.y == o1.y) {
-                return o1.x - o2.x;
-            }
+        Arrays.sort(nodes, (o1, o2)->{
+            if (o1[1] == o2[1]) return Integer.compare(o1[0], o2[0]);
             
-            return o2.y - o1.y; 
+            return Integer.compare(o2[1], o1[1]);
         });
         
-        Node root = tree.get(0);
-        
-        for (int i = 1; i < tree.size(); i++) {
-            insert(root, tree.get(i));
+        Node root = new Node(nodes[0][0], nodes[0][1], nodes[0][2]);
+
+        for (int i = 1; i < N; i++) {
+            insert(root, new Node(nodes[i][0], nodes[i][1], nodes[i][2]));
         }
         
-        preorderResult = new ArrayList<>();
-        postorderResult = new ArrayList<>();
-        
+        preorderList = new ArrayList<>();
+        postorderList = new ArrayList<>();
         preorder(root);
         postorder(root);
         
-        int[][] answer = new int[2][tree.size()];
+        int[][] answer = new int[2][N];
         
-        for (int i = 0; i < tree.size(); i++) {
-            answer[0][i] = preorderResult.get(i);
-            answer[1][i] = postorderResult.get(i);
+        for (int i = 0; i < N; i++) {
+            answer[0][i] = preorderList.get(i);
         }
-
+        
+        for (int i = 0; i < N; i++) {
+            answer[1][i] = postorderList.get(i);
+        }        
+        
         return answer;
     }
 }
