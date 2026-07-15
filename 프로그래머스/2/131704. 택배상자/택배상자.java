c031c4bf780cecb4961729043582468d80cc80fd
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-07-16 00:56:17 +0900

    [level 2] Title: 택배상자, Time: 30.85 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,27 +1,40 @@
 import java.util.*;
 
+/*
+보조 컨베이어 벨트 stack
+
+현재 가지고 있는 상자를 컨베이어 벨트에 넣을지 말지 결정 해야겠다.
+
+*/
 class Solution {
     public int solution(int[] order) {
+        
+        int box = 0;
         int answer = 0;
         ArrayDeque<Integer> dq = new ArrayDeque<>();
         
-        int idx = 0;
-        
         for (int i = 0; i < order.length; i++) {
-            dq.addLast(i+1);
+            boolean flag = false;
             
-            while (!dq.isEmpty()) {
-                if (dq.getLast() == order[idx]) {
-                    idx++;
+            while (true) {
+                if (!dq.isEmpty() && dq.getLast() == order[i]) {
                     answer++;
                     dq.pollLast();
-                } else if (dq.getLast() > order[idx]) {
-                    return answer;
-                } else {
                     break;
                 }
+                
+                box++;
+                
+                if (box > order.length) {
+                    flag = true;
+                    break;
+                }
+                
+                dq.addLast(box);                
             }
-        }
+            
+            if (flag) break;
+        }     
         
         return answer;
     }
