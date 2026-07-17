8e765916a91d9e1d6e2edd12c2c5c4252e4b8789
DaeSeong <39954601+tfer2442@users.noreply.github.com>
2026-07-17 13:31:55 +0900

    [D8] Title: Baby-gin_실습, Time: 70 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D8/16546. Baby－gin＿실습/Baby－gin＿실습.java
+++ SWEA/D8/16546. Baby－gin＿실습/Baby－gin＿실습.java
@@ -1,66 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
-class Solution {
-	public static int n;
-	public static String str1;
-	public static char[] arr;
-	public static int[] visited;
-	public static boolean flag;
+public class Solution {
+	public static int T;
 	
-	public static boolean checkOk(int[] nums) {
-		if (nums[0] == nums[1] && nums[1] == nums[2]) {
-			return true;
-		} 
-		
-		if (nums[0] == nums[1]-1 && nums[1] == nums[2]-1) {
-			return true;
-		}
-		return false;
-	}
-	
-	public static void perm(int cnt) {
-		if (flag) {
-			return;
-		}
-		
-		if (cnt == 6) {
-			int[] nums1 = new int[] {arr[0]-'0', arr[1]-'0', arr[2]-'0'};
-			int[] nums2 = new int[] {arr[3]-'0', arr[4]-'0', arr[5]-'0'};
-			Arrays.sort(nums1);
-			Arrays.sort(nums2);
-			
-			if (checkOk(nums1) && checkOk(nums2)) {
-				flag = true;
-			}
-		}
-		
-		for (int i = 0; i < 6; i++) {
-			if (visited[i] == 1) continue;
-			arr[cnt] = str1.charAt(i);
-			visited[i] = 1;
-			perm(cnt+1);
-			visited[i] = 0;
-		}
-		
-	}
-
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
+		T = Integer.parseInt(br.readLine());
 		
-		for (int T = 1; T <= n; T++) {
-			str1 = br.readLine();
-			arr = new char[6];
-			visited = new int[6];
-			flag = false;
-			perm(0);
-			System.out.println("#" + T + " " + flag);
+		for (int t = 1; t <= T; t++) {
+			String line = br.readLine();
+			HashMap<Integer, Integer> hm = new HashMap<>();
 			
+			for (int i = 0; i < line.length(); i++) {
+				char c = line.charAt(i);
+				hm.put(c-'0', hm.getOrDefault(c-'0', 0)+1);
+			}
+			
+			boolean flag = false;
+			int cnt = 0;
+			int i = 0;
+			
+			while (i < 10) {
+				if (cnt == 6) {
+					flag = true;
+					break;
+				}
+				
+				if (hm.getOrDefault(i, 0) == 0) {
+					i++;
+					continue;
+				}
+				
+				if (hm.get(i) >= 3) {
+					cnt += 3 * (hm.get(i)/3);
+					hm.put(i, hm.get(i)%3);				
+				} else {
+					if (i >= 8) break;
+					
+					if (hm.getOrDefault(i, 0) >= 1 && hm.getOrDefault(i+1, 0) >= 1 && hm.getOrDefault(i+2, 0) >= 1) {					
+						cnt += 3;
+						hm.put(i, hm.get(i)-1);
+						hm.put(i+1, hm.get(i+1)-1);
+						hm.put(i+2, hm.get(i+2)-1);
+					} else {
+						break;
+					}
+				}
+			}
+			
+			System.out.println("#" + t + " " + flag);
 		}
 	}
-
 }
